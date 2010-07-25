bfe2d7a259a680c359344fabe8635a8d72ee50b7
Brad Fitzpatrick <brad@danga.com>
2010-07-25 10:39:08 -0700

    add forgotten file from earlier commits

---
+++ clients/android/uploader/src/com/danga/camli/DummyNullCallback.java
@@ -0,0 +1,47 @@
+package com.danga.camli;
+
+import android.os.RemoteException;
+
+/**
+ * No-op callback for service to use when it doesn't have a real callback.
+ * Avoids a lot of null checks.
+ */
+public class DummyNullCallback extends IStatusCallback.Stub {
+
+    private static final IStatusCallback.Stub mInstance = new DummyNullCallback();
+
+    public static IStatusCallback.Stub instance() {
+        return mInstance;
+    }
+
+    public void logToClient(String stuff) throws RemoteException {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void setBlobStatus(int done, int total) throws RemoteException {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void setBlobsRemain(int num) throws RemoteException {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void setByteStatus(long done, long total) throws RemoteException {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void setUploading(boolean uploading) throws RemoteException {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void setUploadStatusText(String text) throws RemoteException {
+        // TODO Auto-generated method stub
+
+    }
+
+}
