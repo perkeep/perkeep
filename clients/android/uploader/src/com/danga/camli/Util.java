46abac4d4351c43cbbd8963d4bedbada4e850eeb
Brad Fitzpatrick <brad@danga.com>
2010-07-25 10:47:00 -0700

    remove unused hexification code

--- clients/android/uploader/src/com/danga/camli/Util.java
+++ clients/android/uploader/src/com/danga/camli/Util.java
@@ -18,22 +18,6 @@ public class Util {
         return sb.toString();
     }
 
-    private static String convertToHex(byte[] data) {
-        StringBuilder buf = new StringBuilder();
-        for (int i = 0; i < data.length; i++) {
-            int halfbyte = (data[i] >>> 4) & 0x0F;
-            int two_halfs = 0;
-            do {
-                if ((0 <= halfbyte) && (halfbyte <= 9))
-                    buf.append((char) ('0' + halfbyte));
-                else
-                    buf.append((char) ('a' + (halfbyte - 10)));
-                halfbyte = data[i] & 0x0F;
-            } while (two_halfs++ < 1);
-        }
-        return buf.toString();
-    }
-
     private static final String HEX = "0123456789abcdef";
 
     private static String getHex(byte[] raw) {
