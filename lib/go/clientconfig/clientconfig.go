5de1e6c1635f86086689c029200a6c9949f89b0d
Brad Fitzpatrick <brad@danga.com>
2010-12-20 14:54:41 -0800

    Start of little shared camli/clientconfig library

---
+++ lib/go/clientconfig/clientconfig.go
@@ -0,0 +1,41 @@
+package clientconfig
+
+import (
+	"flag"
+	"log"
+	"strings"
+)
+
+// These override the JSON config file ~/.camlistore's "server" and
+// "password" keys
+var flagServer *string = flag.String("blobserver", "", "camlistore blob server")
+var flagPassword *string = flag.String("password", "", "password for blob server")
+
+func cleanServer(server string) string {
+	// Remove trailing slash if provided.
+	if strings.HasSuffix(server, "/") {
+		server = server[0 : len(server)-1]
+	}
+	// Add "http://" prefix if not present:
+	if !strings.HasPrefix(server, "http") {
+		server = "http://" + server
+	}
+	return server
+}
+
+func BlobServerOrDie() string {
+	if *flagServer != "" {
+		return cleanServer(*flagServer)
+	}
+	log.Exitf("No --blobserver parameter specified.")
+	return ""
+}
+
+func PasswordOrDie() string {
+	if *flagPassword != "" {
+		return *flagPassword
+	}
+	log.Exitf("No --password parameter specified.")
+	return ""
+}
+
