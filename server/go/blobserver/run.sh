3b28ed2938ecbbd476d3bbc87bc2a747aedf3006
Brad Fitzpatrick <brad@danga.com>
2011-01-13 18:07:21 -0800

    Clean up run.sh; ignore pwd.

--- server/go/blobserver/run.sh
+++ server/go/blobserver/run.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-mkdir /tmp/camliroot
+Bin=$(dirname $( readlink -f $0))
+
+echo "BIN is: [$Bin]"
+
+ROOT=/tmp/camliroot
+if [ ! -d $ROOT ]; then
+    mkdir $ROOT
+fi
 export CAMLI_PASSWORD=foo
-make && ./camlistored -listen=:3179 "$@"
+
+$Bin/../../../build.pl server/go/blobserver && $Bin/camlistored -root=$ROOT -listen=:3179 "$@"
