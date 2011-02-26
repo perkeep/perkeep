ac7b64eb56ded1312e4d8f788c82fa91b56ea45f
Brad Fitzpatrick <brad@danga.com>
2011-02-26 13:28:22 -0800

    Fix test file's package.  Still no tests.

--- lib/go/httprange/range_test.go
+++ lib/go/httprange/range_test.go
@@ -14,10 +14,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package main
+package httprange
 
 import "testing"
 
-func testRange(t *t.Testing) {
+func TestRange(t *testing.T) {
 
 }
