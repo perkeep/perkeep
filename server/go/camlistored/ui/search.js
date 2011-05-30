5c7e645e7a2cf23e211fa2afda3f96303c23d66a
Brad Fitzpatrick <brad@danga.com>
2011-05-30 13:00:15 -0700

    ui: move old debug search func to its own file

---
+++ server/go/camlistored/ui/search.js
@@ -0,0 +1,14 @@
+function search() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState != 4) { return; }
+        if (xhr.status != 200) {
+            console.log("no status 200; got " + xhr.status);
+            return;
+        }
+        document.getElementById("searchres").innerHTML = "<pre>" + xhr.responseText + "</pre>";
+    };
+    xhr.open("GET", disco.searchRoot + "camli/search", true);
+    xhr.send();
+}
+
--- server/go/camlistored/ui/ui.js
+++ server/go/camlistored/ui/ui.js
@@ -53,6 +53,8 @@ function saneOpts(opts) {
 
 var cachedCamliSigDiscovery;
 
+// opts.success called with discovery object
+// opts.fail called with error text
 function camliSigDiscovery(opts) {
     opts = saneOpts(opts);
     if (cachedCamliSigDiscovery) {
@@ -104,20 +106,6 @@ function camliSign(clearObj, opts) {
                 opts.fail(errMsg);
             }
         });
-}
-
-function search() {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState != 4) { return; }
-        if (xhr.status != 200) {
-            console.log("no status 200; got " + xhr.status);
-            return;
-        }
-        document.getElementById("searchres").innerHTML = "<pre>" + xhr.responseText + "</pre>";
-    };
-    xhr.open("GET", disco.searchRoot + "camli/search", true);
-    xhr.send();
 }
 
 function camliUploadString(s, opts) {
