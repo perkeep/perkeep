65910dcec42f02b162e4d2754be1b1a727c10618
Brad Fitzpatrick <brad@danga.com>
2011-06-09 16:31:53 -0700

    consolidate redundant javascript.

--- server/go/camlistored/ui/camli.js
+++ server/go/camlistored/ui/camli.js
@@ -59,62 +59,26 @@ function camliSigDiscovery(opts) {
         opts.success(cachedCamliSigDiscovery);
         return;
     }
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState != 4) { return; }
-        if (xhr.status != 200) {
-            opts.fail("camliSigDiscovery expected status 200; got " + xhr.status);
-            return;
-        }
-        sigdisco = JSON.parse(xhr.responseText);
-        cachedCamliSigDiscovery = sigdisco;
-        opts.success(sigdisco);
+    var cb = {};
+    cb.success = function(sd) {
+      cachedCamliSigDiscovery = sd;
+      opts.success(sd);
     };
+    cb.fail = opts.fail;
+    var xhr = camliJsonXhr("camliDescribeBlob", cb);
     xhr.open("GET", disco.jsonSignRoot + "/camli/sig/discovery", true);
     xhr.send();
 }
 
 function camliDescribeBlob(blobref, opts) {
-    opts = saneOpts(opts);
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState != 4) { return; }
-        if (xhr.status != 200) {
-            opts.fail("got HTTP status " + xhr.status);
-            return;
-        }
-        var jres;
-        try {
-            jres = JSON.parse(xhr.responseText);
-        } catch (x) {
-            opts.fail("JSON parse error");
-            return;
-        }
-        opts.success(jres);
-    };
+    var xhr = camliJsonXhr("camliDescribeBlob", opts);
     var path = disco.searchRoot + "camli/search/describe?blobref=" + blobref;
     xhr.open("GET", path, true);
     xhr.send();
 }
 
 function camliGetPermanodeClaims(permanode, opts) {
-    opts = saneOpts(opts);
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState != 4) { return; }
-        if (xhr.status != 200) {
-            opts.fail("got HTTP status " + xhr.status);
-            return;
-        }
-        var jres;
-        try {
-            jres = JSON.parse(xhr.responseText);
-        } catch (x) {
-            opts.fail("JSON parse error: " + xhr.responseText);
-            return;
-        }
-        opts.success(jres);
-    };
+    var xhr = camliJsonXhr("camliGetPermanodeClaims", opts);
     var path = disco.searchRoot + "camli/search/claims?permanode=" + permanode;
     xhr.open("GET", path, true);
     xhr.send();
@@ -126,7 +90,7 @@ function camliGetBlobContents(blobref, opts) {
     xhr.onreadystatechange = function() {
         if (xhr.readyState != 4) { return; }
         if (xhr.status != 200) {
-            opts.fail("got HTTP status " + xhr.status);
+            opts.fail("camliGetBlobContents HTTP status " + xhr.status);
             return;
         }
         opts.success(xhr.responseText);
@@ -179,30 +143,8 @@ function camliUploadFileHelper(file, opts) {
     }
     var fd = new FormData();
     fd.append(fd, file);
-    var xhr = new XMLHttpRequest();
-
+    var xhr = camliJsonXhr("camliUploadFileHelper", opts);
     xhr.open("POST", disco.uploadHelper);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState != 4) {
-             return;
-        }
-        if (xhr.status != 200) {
-            opts.fail("got status " + xhr.status);
-            return;
-        }
-        var resj;
-        try {
-            resj = JSON.parse(xhr.responseText);
-        } catch (x) {
-            opts.fail("error parsing JSON in upload response: " + xhr.responseText);
-            return;
-        }
-        if (resj.error) {
-            opts.fail("error uploading " + blobref + ": " + resj.error);
-            return;
-        }
-        opts.success(resj);
-    };
     xhr.send(fd);
 }
 
@@ -217,36 +159,19 @@ function camliUploadString(s, opts) {
     var fd = new FormData();
     fd.append(blobref, bb.getBlob());
     
-    var xhr = new XMLHttpRequest();
-
+    var uploadCb = {};
+    uploadCb.success = function(resj) {
+        // TODO: check resj.received[] array.
+        opts.success(blobref);
+    };
+    uploadCb.fail = opts.fail; 
+    var xhr = camliJsonXhr("camliUploadString", uploadCb);
     // TODO: hack, hard-coding the upload URL here.
     // Change the spec now that App Engine permits 32 MB requests
     // and permit a PUT request on the sha1?  Or at least let us
     // specify the well-known upload URL?  In cases like this, uploading
     // a new permanode, it's silly to even stat.
     xhr.open("POST", disco.blobRoot + "camli/upload")
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState != 4) {
-             return;
-        }
-        if (xhr.status != 200) {
-            opts.fail("got status " + xhr.status);
-            return;
-        }
-        var resj;
-        try {
-            resj = JSON.parse(xhr.responseText);
-        } catch (x) {
-            opts.fail("error parsing JSON in upload response: " + xhr.responseText);
-            return;
-        }
-        if (resj.errorText) {  // TODO: change this to error, not errorText, to be consistent
-            opts.fail("error uploading " + blobref + ": " + resj.errorText);
-            return;
-        }
-        // TODO: check resj.received[] array.
-        opts.success(blobref);
-    };
     xhr.send(fd);
 }
 
@@ -287,34 +212,23 @@ function getQueryParam(key) {
 }
 
 function camliGetRecentlyUpdatedPermanodes(opts) {
+    var xhr = camliJsonXhr("camliGetRecentlyUpdatedPermanodes", opts);
+    xhr.open("GET", disco.searchRoot + "camli/search", true);
+    xhr.send();
+}
+
+function camliJsonXhr(name, opts) {
     opts = saneOpts(opts);
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState != 4) { return; }
         if (xhr.status != 200) {
-            opts.fail("camliGetRecentlyUpdatedPermanodes expected status 200; got " + xhr.status);
-            return;
-        }
-        var resj;
-        try {
-            resj = JSON.parse(xhr.responseText);
-        } catch(x) {
-            opts.fail("error parsing JSON in response: " + xhr.responseText);
-            return
-        }
-        opts.success(resj);
-    };
-    xhr.open("GET", disco.searchRoot + "camli/search", true);
-    xhr.send();
-}
-
-function camliFindExistingFileSchemas(bytesRef, opts) {
-    opts = saneOpts(opts);
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState != 4) { return; }
-        if (xhr.status != 200) {            
-            opts.fail("camliFindExistingFileSchemas expected status 200; got " + xhr.status + ", " + xhr.statusText);
+            try {
+                var resj = JSON.parse(xhr.responseText);
+                opts.fail(name + ": expected status 200; got " + xhr.status + ": " + resj.error);
+            } catch(x) {
+                opts.fail(name + ": expected status 200; got " + xhr.status + ": " + xhr.responseText);
+            }
             return;
         }
         var resj;
@@ -330,6 +244,11 @@ function camliFindExistingFileSchemas(bytesRef, opts) {
             opts.success(resj);
         }
     };
+    return xhr;
+}
+
+function camliFindExistingFileSchemas(bytesRef, opts) {
+    var xhr = camliJsonXhr("camliFindExistingFileSchemas", opts);
     var path = disco.searchRoot + "camli/search/files?bytesref=" + bytesRef;
     xhr.open("GET", path, true);
     xhr.send();
