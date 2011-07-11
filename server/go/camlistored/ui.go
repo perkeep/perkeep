bce36761a1f2a55d982cb99386d2a12d6965c6fb
mpl <mathieu.lonjaret@gmail.com>
2011-07-11 10:50:52 +0200

    undo merging of change If30e0a9613db42a94feda66bb225a844de6d1839 because still a wip
    Change-Id: Id25f5aa0ae63b9d41d8a3ac9fd832194eafd07ee

--- server/go/camlistored/ui.go
+++ server/go/camlistored/ui.go
@@ -47,7 +47,6 @@ var uiFiles = uistatic.Files
 //       if sane looking
 var downloadPattern = regexp.MustCompile(`^download/([^/]+)(/.*)?$`)
 var thumbnailPattern = regexp.MustCompile(`^thumbnail/([^/]+)(/.*)?$`)
-var treePattern = regexp.MustCompile(`^tree/([^/]+)(/.*)?$`)
 
 // UIHandler handles serving the UI and discovery JSON.
 type UIHandler struct {
@@ -176,10 +175,6 @@ func wantsBlobInfo(req *http.Request) bool {
 	return req.Method == "GET" && blobref.Parse(req.FormValue("b")) != nil
 }
 
-func wantsFileTreePage(req *http.Request) bool {
-	return req.Method == "GET" && blobref.Parse(req.FormValue("d")) != nil
-}
-
 func (ui *UIHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	base := req.Header.Get("X-PrefixHandler-PathBase")
 	suffix := req.Header.Get("X-PrefixHandler-PathSuffix")
@@ -194,8 +189,6 @@ func (ui *UIHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		ui.serveDownload(rw, req)
 	case strings.HasPrefix(suffix, "thumbnail/"):
 		ui.serveThumbnail(rw, req)
-	case strings.HasPrefix(suffix, "tree/"):
-		ui.serveFileTree(rw, req)
 	default:
 		file := ""
 		if m := staticFilePattern.FindStringSubmatch(suffix); m != nil {
@@ -208,8 +201,6 @@ func (ui *UIHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 				file = "gallery.html"
 			case wantsBlobInfo(req):
 				file = "blobinfo.html"
-			case wantsFileTreePage(req):
-				file = "filetree.html"
 			case req.URL.Path == base:
 				file = "home.html"
 			default:
@@ -327,30 +318,4 @@ func (ui *UIHandler) serveThumbnail(rw http.ResponseWriter, req *http.Request) {
 		MaxHeight: height,
 	}
 	th.ServeHTTP(rw, req, blobref)
-}
-
-func (ui *UIHandler) serveFileTree(rw http.ResponseWriter, req *http.Request) {
-	if ui.Storage == nil {
-		http.Error(rw, "No BlobRoot configured", 500)
-		return
-	}
-
-	suffix := req.Header.Get("X-PrefixHandler-PathSuffix")
-	m := treePattern.FindStringSubmatch(suffix)
-	if m == nil {
-		httputil.ErrorRouting(rw, req)
-		return
-	}
-
-	blobref := blobref.Parse(m[1])
-	if blobref == nil {
-		http.Error(rw, "Invalid blobref", 400)
-		return
-	}
-
-	fth := &FileTreeHandler{
-		Fetcher:   ui.Storage,
-		file: blobref,
-	}
-	fth.ServeHTTP(rw, req)
 }
