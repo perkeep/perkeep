42dcd20ccb359be8806e98e38dd81e1fbaf95cb7
mpl <mathieu.lonjaret@gmail.com>
2011-07-17 17:50:55 +0200

    browsing a (permanoded) dir continuation of https://camlistore.org/r/36
    Change-Id: Idd4977bb5f4d3c5bf80831ffa4d09085485da0f0

--- server/go/camlistored/ui.go
+++ server/go/camlistored/ui.go
@@ -47,6 +47,7 @@ var uiFiles = uistatic.Files
 //       if sane looking
 var downloadPattern = regexp.MustCompile(`^download/([^/]+)(/.*)?$`)
 var thumbnailPattern = regexp.MustCompile(`^thumbnail/([^/]+)(/.*)?$`)
+var treePattern = regexp.MustCompile(`^tree/([^/]+)(/.*)?$`)
 
 // UIHandler handles serving the UI and discovery JSON.
 type UIHandler struct {
@@ -175,6 +176,10 @@ func wantsBlobInfo(req *http.Request) bool {
 	return req.Method == "GET" && blobref.Parse(req.FormValue("b")) != nil
 }
 
+func wantsFileTreePage(req *http.Request) bool {
+	return req.Method == "GET" && blobref.Parse(req.FormValue("d")) != nil
+}
+
 func (ui *UIHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	base := req.Header.Get("X-PrefixHandler-PathBase")
 	suffix := req.Header.Get("X-PrefixHandler-PathSuffix")
@@ -189,6 +194,8 @@ func (ui *UIHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		ui.serveDownload(rw, req)
 	case strings.HasPrefix(suffix, "thumbnail/"):
 		ui.serveThumbnail(rw, req)
+	case strings.HasPrefix(suffix, "tree/"):
+		ui.serveFileTree(rw, req)
 	default:
 		file := ""
 		if m := staticFilePattern.FindStringSubmatch(suffix); m != nil {
@@ -201,6 +208,8 @@ func (ui *UIHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 				file = "gallery.html"
 			case wantsBlobInfo(req):
 				file = "blobinfo.html"
+			case wantsFileTreePage(req):
+				file = "filetree.html"
 			case req.URL.Path == base:
 				file = "home.html"
 			default:
@@ -250,6 +259,7 @@ func (ui *UIHandler) serveDiscovery(rw http.ResponseWriter, req *http.Request) {
 		"jsonSignRoot":   ui.JSONSignRoot,
 		"uploadHelper":   "?camli.mode=uploadhelper", // hack; remove with better javascript
 		"downloadHelper": "./download/",
+		"directoryHelper": "./tree/",
 		"publishRoots":   pubRoots,
 	})
 }
@@ -318,4 +328,30 @@ func (ui *UIHandler) serveThumbnail(rw http.ResponseWriter, req *http.Request) {
 		MaxHeight: height,
 	}
 	th.ServeHTTP(rw, req, blobref)
+}
+
+func (ui *UIHandler) serveFileTree(rw http.ResponseWriter, req *http.Request) {
+	if ui.Storage == nil {
+		http.Error(rw, "No BlobRoot configured", 500)
+		return
+	}
+
+	suffix := req.Header.Get("X-PrefixHandler-PathSuffix")
+	m := treePattern.FindStringSubmatch(suffix)
+	if m == nil {
+		httputil.ErrorRouting(rw, req)
+		return
+	}
+
+	blobref := blobref.Parse(m[1])
+	if blobref == nil {
+		http.Error(rw, "Invalid blobref", 400)
+		return
+	}
+
+	fth := &FileTreeHandler{
+		Fetcher:   ui.Storage,
+		file: blobref,
+	}
+	fth.ServeHTTP(rw, req)
 }
