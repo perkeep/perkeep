d1f82c3464cea5ee4d6e39265077cbf93e7e78d6
Brad Fitzpatrick <brad@danga.com>
2011-07-20 18:27:38 -0700

    sqlite: add place to put windows stuff
    Change-Id: I53dd835401f824af8f2129a66016f0d23fcde793

--- misc/sqlite/go-sqlite.h
+++ misc/sqlite/go-sqlite.h
@@ -1,4 +1,10 @@
-#define SQLITE_OS_OTHER 1
+#if defined(_WIN32) || defined(WIN32) || defined(__CYGWIN__) || defined(__MINGW32__)
+#  error Not working on Windows yet
+#elif defined(__unix__)
+#  define SQLITE_OS_OTHER 1
+#else
+#  error Unsupported OS
+#endif
 
 #ifndef INCLUDE_SQLITE_VERSION_SYMBOL
 #define SKIP_SQLITE_VERSION
