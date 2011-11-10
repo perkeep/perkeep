cc85ec5bdac310aa2799a817b79a846d3c0766b8
Brad Fitzpatrick <brad@danga.com>
2011-11-10 09:47:32 -0800

    start of enumerate and stat for generic indexer
    Change-Id: Iabac9959a239d2418ab351e37895c7f0721e4e58

---
+++ lib/go/camli/index/enumstat.go
@@ -0,0 +1,34 @@
+/*
+Copyright 2011 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package index
+
+import (
+	"os"
+
+	"camli/blobref"
+)
+
+func (ix *Index) EnumerateBlobs(dest chan<- blobref.SizedBlobRef, after string, limit uint, waitSeconds int) os.Error {
+	defer close(dest)
+	panic("TODO")
+	return nil
+}
+
+func (ix *Index) StatBlobs(dest chan<- blobref.SizedBlobRef, blobs []*blobref.BlobRef, waitSeconds int) os.Error {
+	panic("TODO")
+	return nil
+}
