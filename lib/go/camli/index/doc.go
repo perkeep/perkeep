5ea8cf8d26de3550be02f54900ce4b1f0a380c99
Brad Fitzpatrick <brad@danga.com>
2011-11-27 11:36:30 -0500

    index: add doc, notes, reverse time, start of tests.
    Change-Id: I13bae252be9616f39decbfb4cc60eaa92c0f3db9

---
+++ lib/go/camli/index/doc.go
@@ -0,0 +1,32 @@
+/*
+Copyright 2011 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*
+Package index provides a generic indexing system on top of an abstract
+index storage system (IndexStorage).
+
+The following keys & values are populated by receiving blobs and queried
+for search operations:
+
+* Recent Permanodes
+  "recpn:<signer-blobref>:<reverse-modtime>:<claim-blobref>" == "<permanode-blobref>"
+   where reverse-modtime flips each digit to '9'-<digit> and prepends "rt" (for reverse time)
+          "2011-11-27T01:23:45Z"
+    ==> "rt7988-88-72T98:76:54Z"
+
+
+*/
+package index
