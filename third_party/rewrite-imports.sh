77ac4173ae5808803f9e09e845b2f210eabb118f
Brad Fitzpatrick <brad@danga.com>
2012-02-21 01:55:03 -0800

    fix rewrite-imports.sh destination path
    Change-Id: If6bab560e80593d4f032fa9b19e10b43b365d551

--- third_party/rewrite-imports.sh
+++ third_party/rewrite-imports.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-find . -type f -name '*.go' -exec perl -pi -e 's!"code.google.com/!"camlistore/third_party/code.google.com/!' {} \;
-
+find . -type f -name '*.go' -exec perl -pi -e 's!"code.google.com/!"camlistore.org/third_party/code.google.com/!' {} \;
