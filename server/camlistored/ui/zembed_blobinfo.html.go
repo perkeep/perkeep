f4dd8574773e538c771e67ce242c643691346247
Brad Fitzpatrick <brad@danga.com>
2012-02-28 14:51:21 +1100

    checking in auto-generated zembed_*.go files

---
+++ server/camlistored/ui/zembed_blobinfo.html.go
@@ -0,0 +1,6 @@
+// THIS FILE IS AUTO-GENERATED FROM blobinfo.html
+// DO NOT EDIT.
+package ui
+func init() {
+	Files.Add("blobinfo.html", "<html>\n<head>\n  <title>Blob info</title>\n  <script src=\"camli.js\"></script>\n  <script src=\"?camli.mode=config&cb=onConfiguration\"></script>\n  <script src=\"blobinfo.js\"></script>\n  <link rel=\"stylesheet\" href=\"camli.css\">\n</head>\n<body class=\"camli-ui-blobinfo\">\n  <div class=\"camli-nav\"><a href=\"./\">Home</a></div>\n  <h1>Blob Contents</h1>\n\n  <div id=\"thumbnail\"></div>\n  <span id=\"editspan\" class=\"camli-nav\" style=\"display: none;\"><a href=\"#\" id=\"editlink\">edit</a></span>\n  <span id=\"blobdownload\" class=\"camli-nav\"></span>\n  <span id=\"blobdescribe\" class=\"camli-nav\"></span>\n  <span id=\"blobbrowse\" class=\"camli-nav\"></span>\n\n  <pre id=\"blobdata\"></pre>\n\n  <h1>Indexer Metadata</h1>\n  <pre id=\"blobmeta\"></pre>\n\n  <div id=\"claimsdiv\" style=\"visibility: hidden\">\n    <h1>Mutation Claims</h1>\n    <pre id=\"claims\"></pre>\n  </div>\n\n</body>\n</html>\n");
+}
---
+++ server/camlistored/ui/zembed_filetree.html.go
@@ -0,0 +1,6 @@
+// THIS FILE IS AUTO-GENERATED FROM filetree.html
+// DO NOT EDIT.
+package ui
+func init() {
+	Files.Add("filetree.html", "<!doctype html>\n<html>\n<head>\n  <title>Gallery</title>\n  <script src=\"base64.js\"></script>\n  <script src=\"Crypto.js\"></script>\n  <script src=\"SHA1.js\"></script>\n  <script src=\"camli.js\"></script>\n  <script src=\"?camli.mode=config&cb=onConfiguration\"></script>\n  <script src=\"filetree.js\"></script>\n  <link rel=\"stylesheet\" href=\"camli.css\">\n</head>\n<body class=\"camli-ui-permanode\">\n  <div class=\"camli-nav\"><a href=\"./\">Home</a></div>\n  <h1>FileTree for <span id=\"curDir\" class=\"camli-nav\"></span> </h1>\n\n  <div id=\"children\"></div>\n\n</body>\n</html>\n");
+}
