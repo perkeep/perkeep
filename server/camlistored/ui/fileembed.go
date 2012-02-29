dbf7e5e1e9fcf88175d19b4ee1e14a2d870f2d97
Brad Fitzpatrick <brad@danga.com>
2012-02-29 08:01:32 -0800

    server/camlistored/ui: embed css too
    Change-Id: Ic02b939dd59a4f9378719f18cfe2b5bc50545aab

--- server/camlistored/ui/fileembed.go
+++ server/camlistored/ui/fileembed.go
@@ -15,7 +15,7 @@ limitations under the License.
 */
 
 /*
-#fileembed pattern .+\.(js|html)
+#fileembed pattern .+\.(js|css|html)
 */
 package ui
 
---
+++ server/camlistored/ui/zembed_pics.css.go
@@ -0,0 +1,7 @@
+// THIS FILE IS AUTO-GENERATED FROM pics.css
+// DO NOT EDIT.
+package ui
+import "time"
+func init() {
+	Files.Add("pics.css", "/* Something arbitrary for testing. */\nbody {\n  font: 13px/1.3 normal Verdana, Geneva, sans-serif;\n  background: #000;\n  color: #aaa;\n  margin: 0;\n  padding: 30px;\n}\na {\n  color: #aaa;\n}\na:hover {\n  color: #bbb;\n}\nh1 {\n  border: 3px dashed #aaa;\n  padding: 1em;\n}\nul {\n  list-style: none;\n  background: #262626;\n\n  margin: 0;\n  padding: 20px;\n  border-radius: 10px;\n}\nli {\n  display: inline-block;\n  vertical-align: top;\n  padding: 1em;\n  margin: 1em;\n  text-align: right;\n}\nli:hover {\n  background: #000;\n  border-radius: 10px;\n  border-bottom-left-radius: 0;\n  border-bottom-right-radius: 0;\n}\nli:hover img {\n  xmax-height: none;\n  xmax-width: none;\n}\nli a {\n  font-weight: bold;\n}\nli img {\n  border: 0;\n  border-radius: 6px;\n  display: block;\n  max-height: 200px;\n  max-width: 200px;\n  margin-bottom: 1em;\n}\nli .camlifile {\n  display: none;\n}\nli a span {\n  padding: 2px;\n  border: 1px solid transparent;\n}\nli input {\n  text-align: right;\n}\n\na.pics-edit,\na.pics-edit:hover {\n  font-size: 70%;\n  color: #f00;\n  margin-right: .5em;\n  font-weight: normal;\n}\n", time.Unix(0, 1310770714000000000));
+}
