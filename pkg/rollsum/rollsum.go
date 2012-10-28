d2b8dc2327751d6a65d73b24c53744e3f83d4665
Brad Fitzpatrick <brad@danga.com>
2012-10-28 13:08:39 +0100

    rollsum: decrease split frequency by 4x
    Change-Id: Ibf945004abff61134fbcd07f47734ea3f0944ba0

--- pkg/rollsum/rollsum.go
+++ pkg/rollsum/rollsum.go
@@ -26,8 +26,8 @@ import ()
 const windowSize = 64
 const charOffset = 31
 
-const blobBits = 13
-const blobSize = 1 << blobBits // 8k
+const blobBits = 15
+const blobSize = 1 << blobBits // 32k
 
 type RollSum struct {
 	s1, s2 uint32
