7b420e7d374692b38ed6ad9865d84cfba920f2cc
Brett Slatkin <bslatkin@gmail.com>
2012-12-23 16:01:15 -0800

    newui: Using spaces instead of tabs in js/html/css
    Change-Id: Icd5a74e080f8802d9d5260270205c4eb39f584eb

--- server/camlistored/newui/home.js
+++
@@ -1,8 +0,0 @@
-goog.provide('home');
-goog.require('goog.dom');
-
-home.hello = function() {
-	var h1 = goog.dom.createDom('h1', {'style': 'background-color:#EEE'},
-		'Welcome to the new camlistore ui');
-	goog.dom.appendChild(document.body, h1);
-}
