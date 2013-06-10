dcc41792fb8a418e3dcb3784102a301b8820cb13
Brad Fitzpatrick <brad@danga.com>
2013-06-11 00:55:31 +0200

    Add a genfileembed package comment
    Change-Id: I65bfd3c1956921bb8d1d805cd6df84bc376ec802

--- pkg/fileembed/genfileembed/genfileembed.go
+++ pkg/fileembed/genfileembed/genfileembed.go
@@ -14,6 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// The genfileembed command embeds resources into Go files, to eliminate run-time
+// dependencies on files on the filesystem.
 package main
 
 import (
