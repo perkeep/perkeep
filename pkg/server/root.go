8b8d573c275daec50c574f690c1e253a48be6a2b
mpl <mathieu.lonjaret@gmail.com>
2013-06-11 15:20:48 +0200

    root page: added link to ui
    Change-Id: Ie9d0f7dba380a72f727ea1ed29685a0a01abfc1c

--- pkg/server/root.go
+++ pkg/server/root.go
@@ -142,11 +142,13 @@ func (rh *RootHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 
 	configLink := ""
 	if auth.IsLocalhost(req) {
-		configLink = "<p>If you're coming from localhost, hit <a href='/setup'>/setup</a>.</p>"
+		configLink = "<p>If you're coming from localhost, configure your Camlistore server at <a href='/setup'>/setup</a>.</p>"
 	}
 	fmt.Fprintf(rw, "<html><body>This is camlistored, a "+
 		"<a href='http://camlistore.org'>Camlistore</a> server."+
-		"%s</body></html>\n", configLink)
+		"%s"+
+		"<p>To manage your content, access the <a href='/ui'>/ui</a>.</p></body></html>\n",
+		configLink)
 }
 
 type byFromTo []*SyncHandler
