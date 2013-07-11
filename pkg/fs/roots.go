ec7e5ca5266fc0c23ecedbc0ff2b92f46a6c0b15
Brad Fitzpatrick <brad@danga.com>
2013-07-11 20:17:27 +1000

    fs: show nice names in the 'roots' directory, not just blobrefs.
    Change-Id: I2aa8ff366160e1ca515d4051d8856313689bc029

--- pkg/fs/roots.go
+++ pkg/fs/roots.go
@@ -21,15 +21,22 @@ package fs
 import (
 	"log"
 	"os"
+	"sync"
+	"time"
 
 	"camlistore.org/pkg/blobref"
 	"camlistore.org/pkg/search"
-
 	"camlistore.org/third_party/code.google.com/p/rsc/fuse"
 )
 
+const refreshTime = 1 * time.Minute
+
 type rootsDir struct {
 	fs *CamliFileSystem
+
+	mu        sync.Mutex // guards following
+	lastQuery time.Time
+	m         map[string]*blobref.BlobRef // ent name => permanode
 }
 
 func (n *rootsDir) Attr() fuse.Attr {
@@ -41,30 +48,26 @@ func (n *rootsDir) Attr() fuse.Attr {
 }
 
 func (n *rootsDir) ReadDir(intr fuse.Intr) ([]fuse.Dirent, fuse.Error) {
-	log.Printf("fs.roots: ReadDir / searching")
-
-	req := &search.WithAttrRequest{N: 100, Attr: "camliRoot"}
-	res, err := n.fs.client.GetPermanodesWithAttr(req)
-	if err != nil {
-		log.Printf("fs.recent: GetRecentPermanodes error in ReadDir: %v", err)
+	n.mu.Lock()
+	defer n.mu.Unlock()
+	if err := n.condRefresh(); err != nil {
 		return nil, fuse.EIO
 	}
-
 	var ents []fuse.Dirent
-	for _, wi := range res.WithAttr {
-		// TODO(adg): do a describe on the permanode so we can figure out its camliRoot value
-		// eg: "camliRoot": ["dev-pics-root"] becomes ent name "dev-pics-root"
-		ents = append(ents, fuse.Dirent{
-			Name: wi.Permanode.String(),
-		})
+	for name := range n.m {
+		ents = append(ents, fuse.Dirent{Name: name})
 	}
-	log.Printf("fs.recent returning %d entries", len(ents))
 	return ents, nil
 }
 
 func (n *rootsDir) Lookup(name string, intr fuse.Intr) (fuse.Node, fuse.Error) {
 	log.Printf("fs.roots: Lookup(%q)", name)
-	br := blobref.Parse(name)
+	n.mu.Lock()
+	defer n.mu.Unlock()
+	if err := n.condRefresh(); err != nil {
+		return nil, err
+	}
+	br := n.m[name]
 	if br == nil {
 		return nil, fuse.ENOENT
 	}
@@ -73,4 +76,46 @@ func (n *rootsDir) Lookup(name string, intr fuse.Intr) (fuse.Node, fuse.Error) {
 		br: br,
 	}
 	return nod, nil
+}
+
+// requires n.mu is held
+func (n *rootsDir) condRefresh() fuse.Error {
+	if n.lastQuery.After(time.Now().Add(-refreshTime)) {
+		return nil
+	}
+	log.Printf("fs.roots: querying")
+
+	req := &search.WithAttrRequest{N: 100, Attr: "camliRoot"}
+	wres, err := n.fs.client.GetPermanodesWithAttr(req)
+	if err != nil {
+		log.Printf("fs.recent: GetRecentPermanodes error in ReadDir: %v", err)
+		return fuse.EIO
+	}
+
+	dr := &search.DescribeRequest{
+		Depth: 1,
+	}
+	for _, wi := range wres.WithAttr {
+		dr.BlobRefs = append(dr.BlobRefs, wi.Permanode)
+	}
+	dres, err := n.fs.client.Describe(dr)
+	if err != nil {
+		log.Printf("Describe failure: %v", err)
+		return fuse.EIO
+	}
+
+	n.m = make(map[string]*blobref.BlobRef)
+
+	for _, wi := range wres.WithAttr {
+		pn := wi.Permanode
+		db := dres.Meta[pn.String()]
+		if db != nil && db.Permanode != nil {
+			name := db.Permanode.Attr.Get("camliRoot")
+			if name != "" {
+				n.m[name] = pn
+			}
+		}
+	}
+	n.lastQuery = time.Now()
+	return nil
 }
