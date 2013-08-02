e4e976e8a10389149c35a21cfae0cb2524a13774
mpl <mathieu.lonjaret@gmail.com>
2013-08-02 21:57:16 +0200

    fs: typo in copyright date
    Change-Id: I59cbf752971d7f1b9a8e6b291aec814d3b833378

--- pkg/fs/mut.go
+++ pkg/fs/mut.go
@@ -1,7 +1,7 @@
 // +build linux darwin
 
 /*
-Copyright 2012 Google Inc.
+Copyright 2013 Google Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
