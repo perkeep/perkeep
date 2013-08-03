7bbc2828fdfc236d4c5ae9eeaf0a89fcf0e73a68
Brad Fitzpatrick <brad@danga.com>
2013-08-03 13:04:33 -0700

    demo.sh script to source during demos
    Change-Id: If7415d16f21fdc4ae16778b3bf2ce5c908067193

---
+++ dev/demo.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Some hacks to make make demoing Camlistore less distracting but
+# still permit using the dev-* scripts (which are normally slow and
+# noisy)
+
+go run make.go
+export CAMLI_QUIET=1
+export CAMLI_FAST_DEV=1
