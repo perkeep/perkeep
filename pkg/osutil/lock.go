d7898d471272b4c9bbf252b212216b356f0618f7
Brad Fitzpatrick <brad@danga.com>
2013-08-23 11:54:25 -0500

    pkg/osutil: add Lock function and tests.
    Prep for using cznic/kv in camput and as index.Storage.
    
    Change-Id: If912f7cb6b33247217f7812ac2082414436c0bf5

---
+++ pkg/osutil/lock.go
@@ -0,0 +1,90 @@
+/*
+Copyright 2013 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package osutil
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"sync"
+)
+
+// Lock locks the given file, creating the file if necessary. If the
+// file already exists, it must have zero size or an error is returned.
+// The lock is an exclusive lock (a write lock), but locked files
+// should neither be read from nor written to. Such files should have
+// zero size and only exist to co-ordinate ownership across processes.
+//
+// A nil Closer is returned if an error occurred. Otherwise, close that
+// Closer to release the lock.
+//
+// On Linux and OSX, a lock has the same semantics as fcntl(2)'s advisory
+// locks.  In particular, closing any other file descriptor for the same
+// file will release the lock prematurely.
+//
+// Attempting to lock a file that is already locked by the current process
+// has undefined behavior.
+//
+// Lock is not yet implemented on other operating systems, and calling it
+// will return an error.
+func Lock(name string) (io.Closer, error) {
+	return lockFn(name)
+}
+
+var lockFn = lockPortable
+
+// Portable version not using fcntl. Doesn't handle crashes as gracefully,
+// since it can leave stale lock files.
+// TODO: write pid of owner to lock file and on race see if pid is
+// still alive?
+func lockPortable(name string) (io.Closer, error) {
+	absName, err := filepath.Abs(name)
+	if err != nil {
+		return nil, fmt.Errorf("can't Lock file %q: can't find abs path: %v", name, err)
+	}
+	fi, err := os.Stat(absName)
+	if err == nil && fi.Size() > 0 {
+		return nil, fmt.Errorf("can't Lock file %q: has non-zero size", name)
+	}
+	f, err := os.OpenFile(absName, os.O_RDWR|os.O_CREATE|os.O_TRUNC|os.O_EXCL, 0666)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create lock file %s %v", absName, err)
+	}
+	return &lockCloser{f: f, abs: absName}, nil
+}
+
+type lockCloser struct {
+	f    *os.File
+	abs  string
+	once sync.Once
+	err  error
+}
+
+func (lc *lockCloser) Close() error {
+	lc.once.Do(lc.close)
+	return lc.err
+}
+
+func (lc *lockCloser) close() {
+	if err := lc.f.Close(); err != nil {
+		lc.err = err
+	}
+	if err := os.Remove(lc.abs); err != nil {
+		lc.err = err
+	}
+}
---
+++ pkg/osutil/lock_test.go
@@ -0,0 +1,110 @@
+/*
+Copyright 2013 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package osutil
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"testing"
+)
+
+func TestLock(t *testing.T) {
+	testLock(t, false)
+}
+
+func TestLockPortable(t *testing.T) {
+	testLock(t, true)
+}
+
+func TestLockInChild(t *testing.T) {
+	f := os.Getenv("TEST_LOCK_FILE")
+	if f == "" {
+		// not child
+		return
+	}
+	lock := Lock
+	if v, _ := strconv.ParseBool(os.Getenv("TEST_LOCK_PORTABLE")); v {
+		lock = lockPortable
+	}
+
+	lk, err := lock(f)
+	if err != nil {
+		log.Fatal("Lock failed: %v", err)
+	}
+	lk.Close()
+}
+
+func testLock(t *testing.T, portable bool) {
+	lock := Lock
+	if portable {
+		lock = lockPortable
+	}
+
+	td, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(td)
+
+	path := filepath.Join(td, "foo.lock")
+
+	childLock := func() error {
+		cmd := exec.Command(os.Args[0], "-test.run=LockInChild$")
+		cmd.Env = []string{"TEST_LOCK_FILE=" + path}
+		if portable {
+			cmd.Env = append(cmd.Env, "TEST_LOCK_PORTABLE=1")
+		}
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			return fmt.Errorf("Child Process lock of %s failed: %v %s", path, err, out)
+		}
+		return nil
+	}
+
+	if err := childLock(); err != nil {
+		t.Fatalf("lock in child process: %v", err)
+	}
+
+	lk1, err := lock(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = lock(path)
+	if err == nil {
+		t.Fatal("expected second lock to fail")
+	}
+
+	if childLock() == nil {
+		t.Fatalf("expected lock in child process to fail")
+	}
+
+	if err := lk1.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	lk3, err := lock(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	lk3.Close()
+}
