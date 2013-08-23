perkeep/perkeep

commit b24cad68ddea101f5720314649386087bcc93a04

Cleanup: remove BlobHub and time.Duration waits from storage interface
Move up a layer to the HTTP.  Also, start to remove ContextWrapper
stuff.  We've done it differently for App Engine instead, and will do
it differently yet moving forward.

Also add blobserver.Receive and use it in most places, moving checksum
verification up a layer.

Bunch of other cleanup and TODO fixing too.

Much simpler and cleaner.

Change-Id: I12e56c5d4e53bfcf82bdd8fb0b6d57c248ff605c

pkg/blobserver/localdisk/stat.go
@@ -18,81 +18,45 @@ package localdisk
 
 import (
 	"os"
-	"sync"
-	"time"
 
 	"camlistore.org/pkg/blob"
 )
 
 const maxParallelStats = 20
 
-func (ds *DiskStorage) StatBlobs(dest chan<- blob.SizedRef, blobs []blob.Ref, wait time.Duration) error {
+func (ds *DiskStorage) StatBlobs(dest chan<- blob.SizedRef, blobs []blob.Ref) error {
 	if len(blobs) == 0 {
 		return nil
 	}
 
-	var missingLock sync.Mutex
-	var missing []blob.Ref
-
-	statSend := func(ref blob.Ref, appendMissing bool) error {
+	statSend := func(ref blob.Ref) error {
 		fi, err := os.Stat(ds.blobPath(ds.partition, ref))
 		switch {
-		case err == nil && !fi.IsDir():
+		case err == nil && fi.Mode().IsRegular():
 			dest <- blob.SizedRef{Ref: ref, Size: fi.Size()}
-		case err != nil && appendMissing && os.IsNotExist(err):
-			missingLock.Lock()
-			missing = append(missing, ref)
-			missingLock.Unlock()
+			return nil
 		case err != nil && !os.IsNotExist(err):
 			return err
 		}
 		return nil
 	}
 
-	// Stat all the files in parallel if there are multiple
 	if len(blobs) == 1 {
-		if err := statSend(blobs[0], wait > 0); err != nil {
-			return err
-		}
-	} else {
-		errCh := make(chan error)
-		rateLimiter := make(chan bool, maxParallelStats)
-		for _, ref := range blobs {
-			go func(ref blob.Ref) {
-				rateLimiter <- true
-				errCh <- statSend(ref, wait > 0)
-				<-rateLimiter
-			}(ref)
-		}
-		for _, _ = range blobs {
-			if err := <-errCh; err != nil {
-				return err
-			}
-		}
+		return statSend(blobs[0])
 	}
 
-	if needed := len(missing); needed > 0 {
-		if wait > 1*time.Minute {
-			// TODO: use a flag, defaulting to a minute?
-			wait = 1 * time.Minute
-		}
-		hub := ds.GetBlobHub()
-		ch := make(chan blob.Ref, 1)
-		for _, missblob := range missing {
-			hub.RegisterBlobListener(missblob, ch)
-			defer hub.UnregisterBlobListener(missblob, ch)
-		}
-		timer := time.NewTimer(wait)
-		defer timer.Stop()
-		select {
-		case <-timer.C:
-			// Done waiting.
-			return nil
-		case blobref := <-ch:
-			statSend(blobref, false)
-			if needed--; needed == 0 {
-				return nil
-			}
+	errCh := make(chan error)
+	rateLimiter := make(chan bool, maxParallelStats)
+	for _, ref := range blobs {
+		rateLimiter <- true
+		go func(ref blob.Ref) {
+			errCh <- statSend(ref)
+			<-rateLimiter
+		}(ref)
+	}
+	for _ = range blobs {
+		if err := <-errCh; err != nil {
+			return err
 		}
 	}
 	return nil

pkg/blobserver/localdisk/localdisk_test.go
@@ -21,7 +21,6 @@ import (
 	"os"
 	"sync"
 	"testing"
-	"time"
 
 	"camlistore.org/pkg/blob"
 	"camlistore.org/pkg/test"
@@ -71,7 +70,7 @@ func TestReceiveStat(t *testing.T) {
 	ch := make(chan blob.SizedRef, 0)
 	errch := make(chan error, 1)
 	go func() {
-		errch <- ds.StatBlobs(ch, tb.BlobRefSlice(), 0)
+		errch <- ds.StatBlobs(ch, tb.BlobRefSlice())
 		close(ch)
 	}()
 	got := 0
@@ -82,40 +81,6 @@ func TestReceiveStat(t *testing.T) {
 	}
 	AssertInt(t, 1, got, "number stat results")
 	AssertNil(t, <-errch, "result from stat")
-}
-
-func TestStatWait(t *testing.T) {
-	ds := NewStorage(t)
-	defer cleanUp(ds)
-	tb := &test.Blob{"Foo"}
-
-	// Do a stat before the blob exists, but wait 2 seconds for it to arrive.
-	wait := 2 * time.Second
-	ch := make(chan blob.SizedRef, 0)
-	errch := make(chan error, 1)
-	go func() {
-		errch <- ds.StatBlobs(ch, tb.BlobRefSlice(), wait)
-		close(ch)
-	}()
-
-	// Sum and verify the stat results, writing the total number of returned matches
-	// to statCountCh (expected: 1)
-	statCountCh := make(chan int)
-	go func() {
-		got := 0
-		for sb := range ch {
-			got++
-			tb.AssertMatches(t, sb)
-		}
-		statCountCh <- got
-	}()
-
-	// Now upload the blob, now that everything else is in-flight.
-	// Sleep a bit to make sure the ds.Stat above has had a chance to fail and sleep.
-	time.Sleep(1e9 / 5) // 200ms in nanos
-	tb.MustUpload(t, ds)
-
-	AssertInt(t, 1, <-statCountCh, "number stat results")
 }
 
 func TestMultiStat(t *testing.T) {
@@ -135,8 +100,7 @@ func TestMultiStat(t *testing.T) {
 	errch := make(chan error, 1)
 	go func() {
 		errch <- ds.StatBlobs(ch,
-			[]blob.Ref{blobfoo.BlobRef(), blobbar.BlobRef()},
-			0)
+			[]blob.Ref{blobfoo.BlobRef(), blobbar.BlobRef()})
 		close(ch)
 	}()
 	got := 0

pkg/blobserver/google/drive/stat.go
@@ -17,12 +17,10 @@ limitations under the License.
 package drive
 
 import (
-	"time"
-
 	"camlistore.org/pkg/blob"
 )
 
-func (sto *driveStorage) StatBlobs(dest chan<- blob.SizedRef, blobs []blob.Ref, wait time.Duration) error {
+func (sto *driveStorage) StatBlobs(dest chan<- blob.SizedRef, blobs []blob.Ref) error {
 	for _, br := range blobs {
 		size, err := sto.service.Stat(br.String())
 		if err == nil {

pkg/blobserver/handlers/stat.go
@@ -41,7 +41,8 @@ func handleStat(conn http.ResponseWriter, req *http.Request, storage blobserver.
 		storage = w.WrapContext(req)
 	}
 
-	toStat := make([]blob.Ref, 0)
+	needStat := map[blob.Ref]bool{}
+
 	switch req.Method {
 	case "POST":
 		fallthrough
@@ -69,7 +70,7 @@ func handleStat(conn http.ResponseWriter, req *http.Request, storage blobserver.
 				httputil.BadRequestError(conn, "Bogus blobref for key "+key)
 				return
 			}
-			toStat = append(toStat, ref)
+			needStat[ref] = true
 		}
 	default:
 		httputil.BadRequestError(conn, "Invalid method.")
@@ -92,11 +93,22 @@ func handleStat(conn http.ResponseWriter, req *http.Request, storage blobserver.
 	}
 
 	statRes := make([]map[string]interface{}, 0)
-	if len(toStat) > 0 {
+	deadline := time.Now().Add(time.Duration(waitSeconds) * time.Second)
+
+	toStat := make([]blob.Ref, 0, len(needStat))
+	buildToStat := func() {
+		toStat = toStat[:0]
+		for br := range needStat {
+			toStat = append(toStat, br)
+		}
+	}
+
+	for len(needStat) > 0 {
+		buildToStat()
 		blobch := make(chan blob.SizedRef)
 		resultch := make(chan error, 1)
 		go func() {
-			err := storage.StatBlobs(blobch, toStat, time.Duration(waitSeconds)*time.Second)
+			err := storage.StatBlobs(blobch, toStat)
 			close(blobch)
 			resultch <- err
 		}()
@@ -106,6 +118,7 @@ func handleStat(conn http.ResponseWriter, req *http.Request, storage blobserver.
 			ah["blobRef"] = sb.Ref.String()
 			ah["size"] = sb.Size
 			statRes = append(statRes, ah)
+			delete(needStat, sb.Ref)
 		}
 
 		err := <-resultch
@@ -114,6 +127,13 @@ func handleStat(conn http.ResponseWriter, req *http.Request, storage blobserver.
 			conn.WriteHeader(http.StatusInternalServerError)
 			return
 		}
+
+		if len(needStat) == 0 || waitSeconds == 0 || time.Now().After(deadline) {
+			break
+		}
+
+		buildToStat()
+		blobserver.WaitForBlob(storage, deadline, toStat)
 	}
 
 	configer, _ := storage.(blobserver.Configer)

pkg/blobserver/s3/stat.go
@@ -17,12 +17,10 @@ limitations under the License.
 package s3
 
 import (
-	"time"
-
 	"camlistore.org/pkg/blob"
 )
 
-func (sto *s3Storage) StatBlobs(dest chan<- blob.SizedRef, blobs []blob.Ref, wait time.Duration) error {
+func (sto *s3Storage) StatBlobs(dest chan<- blob.SizedRef, blobs []blob.Ref) error {
 	// TODO: do n stats in parallel
 	for _, br := range blobs {
 		size, err := sto.s3Client.Stat(br.String(), sto.bucket)

TESTS
@@ -14,3 +14,6 @@ Tests needed
 
 -- pkg/auth -- not enough tests. see regression at
    https://camlistore-review.googlesource.com/#/c/556/1
+
+-- blobserver.WaitForBlob, and integration tests for the http handlers
+   for long-polling on Enumerate and Stat

TODO
@@ -4,31 +4,27 @@ There are two TODO lists. This file (good for airplanes) and the online bug trac
 
 Offline list:
 
+-- verify all ReceiveBlob calls and see which should be blobserver.Receive
+   instead, or ReceiveNoHash.  git grep -E "\.ReceiveBlob\("
+
+-- remove ContextWrapper stuff.
+
 -- union storage target. sharder can be thought of a specialization
    of union. sharder already unions, but has a hard-coded policy
    of where to put new blobs. union could a library (used by sharder)
    with a pluggable policy on that.
 
--- remove all the "if !blob.HashMatches" stuff from the blobserver
-   storage implementations' ReceiveBlob methods. Have the HTTP layer
-   do it instead.  Can give them an io.Reader which does the
-   verification before its final EOF, else returns an ErrCorruptBlob
-   as the final Read call.
-
 -- support for running cammount under camlistored. especially for OS X,
    where the lifetime of the background daemon will be the same as the
    user's login session.
 
--- remove all the blobhub stuff from the blobserver storage interface
-   and move it elsewhere? nobody really implements it anyway, suggesting
-   it's at the wrong level. in the process, also look into simplifying
-   the EnumerateBlobs interface and its "wait" parameter. perhaps we need
-   a new optional AwaitNewBlob method on a storage interface, and the
-   HTTP layer would use that, instead of assuming that all EnumerateBlob
-   impls can wait.
-
 -- verify that the HTTP blob upload interface caps uploads at MaxBlobSize
    before giving it to storage impls
+
+-- add pkg/gate w/ chan(struct {}, size) internally
+
+-- use pkg/gate in s3 Stat method. (there's a TODO there now to stat
+   in parallel)
 
 -- support syncing from a storage type that doesn't support
    QueueCreator by using local disk instead. writes go to both 1) that

cmd/camput/files.go
@@ -307,7 +307,7 @@ func (sr *statsStatReceiver) ReceiveBlob(br blob.Ref, source io.Reader) (sb blob
 	return blob.SizedRef{br, n}, nil
 }
 
-func (sr *statsStatReceiver) StatBlobs(dest chan<- blob.SizedRef, blobs []blob.Ref, _ time.Duration) error {
+func (sr *statsStatReceiver) StatBlobs(dest chan<- blob.SizedRef, blobs []blob.Ref) error {
 	sr.mu.Lock()
 	defer sr.mu.Unlock()
 	for _, br := range blobs {

commit 7dd7f948ccfc349f5fd3e9bfc2f75f3a63ccc496

localdisk: fix Stat deadlock regression, use pkg/gate.
The errc channel needs to be buffered. Broken by b24cad68ddea101f5720314649386087bcc93a04 which
radically simplified this method.

Change-Id: I7f4df7d47cca78d6098000b926d7b734dd712bfa

pkg/blobserver/localdisk/stat.go
@@ -20,6 +20,7 @@ import (
 	"os"
 
 	"camlistore.org/pkg/blob"
+	"camlistore.org/pkg/gate"
 )
 
 const maxParallelStats = 20
@@ -45,17 +46,18 @@ func (ds *DiskStorage) StatBlobs(dest chan<- blob.SizedRef, blobs []blob.Ref) er
 		return statSend(blobs[0])
 	}
 
-	errCh := make(chan error)
-	rateLimiter := make(chan bool, maxParallelStats)
+	errc := make(chan error, len(blobs))
+
+	gt := gate.New(maxParallelStats)
 	for _, ref := range blobs {
-		rateLimiter <- true
+		gt.Start()
 		go func(ref blob.Ref) {
-			errCh <- statSend(ref)
-			<-rateLimiter
+			defer gt.Done()
+			errc <- statSend(ref)
 		}(ref)
 	}
 	for _ = range blobs {
-		if err := <-errCh; err != nil {
+		if err := <-errc; err != nil {
 			return err
 		}
 	}

pkg/blobserver/localdisk/localdisk_test.go
@@ -19,6 +19,7 @@ package localdisk
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"sync"
 	"testing"
 
@@ -96,11 +97,19 @@ func TestMultiStat(t *testing.T) {
 	need[blobfoo.BlobRef().String()] = true
 	need[blobbar.BlobRef().String()] = true
 
+	blobs := []blob.Ref{blobfoo.BlobRef(), blobbar.BlobRef()}
+
+	// In addition to the two "foo" and "bar" blobs, add
+	// maxParallelStats other dummy blobs, to exercise the stat
+	// rate-limiting (which had a deadlock once after a cleanup)
+	for i := 0; i < maxParallelStats; i++ {
+		blobs = append(blobs, blob.SHA1FromString(strconv.Itoa(i)))
+	}
+
 	ch := make(chan blob.SizedRef, 0)
 	errch := make(chan error, 1)
 	go func() {
-		errch <- ds.StatBlobs(ch,
-			[]blob.Ref{blobfoo.BlobRef(), blobbar.BlobRef()})
+		errch <- ds.StatBlobs(ch, blobs)
 		close(ch)
 	}()
 	got := 0