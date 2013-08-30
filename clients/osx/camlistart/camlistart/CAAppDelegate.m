cb40c185f66a29258c057502e6c017fc301acdaf
Nick O'Neill <nick.oneill@gmail.com>
2013-08-30 12:27:45 -0700

    More work on OS X launcher.
    Change-Id: Id88e5f1c26b536b4a296f25b08bb4375ea014dc5

--- clients/osx/camlistart/camlistart/CAAppDelegate.m
+++ clients/osx/camlistart/camlistart/CAAppDelegate.m
@@ -5,13 +5,12 @@
 //  Created by Nick O'Neill on 7/7/13.
 //  Copyright (c) 2013 Camlistore. All rights reserved.
 //
-//  Based on code from Vadim Shpakovski
-//  https://github.com/shpakovski/Popup
-//
 
 #import "CAAppDelegate.h"
 
 @implementation CAAppDelegate
+
+NSString * const plistFile = @"org.camlistore.plist";
 
 @synthesize panelController = _panelController;
 @synthesize menubarController = _menubarController;
@@ -19,13 +18,79 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     self.menubarController = [[MenubarController alloc] init];
+    
+    // start the camlistored process
+    [self launchctlLoad];
 }
 
 - (NSApplicationTerminateReply)applicationShouldTerminate:(NSApplication *)sender
 {
+    // stop the camlistored process
+    [self launchctlUnload];
+
     // Explicitly remove the icon from the menu bar
     self.menubarController = nil;
+    
     return NSTerminateNow;
+}
+
+#pragma mark - launchctl stuff
+
+// tell launchctl to load the plist when we start, it can handle double-starts and other nonsense
+- (void)launchctlLoad
+{
+    NSString *launchPlist = [[self supportDirectory] stringByAppendingPathComponent:plistFile];
+    
+    // remove the existing plist for each launch, we can't count on the bundle always being in the same place
+    if ([[NSFileManager defaultManager] fileExistsAtPath:launchPlist]) {
+        [[NSFileManager defaultManager] removeItemAtPath:launchPlist error:nil];
+    }
+    
+    NSString *bundlePath = [[NSBundle mainBundle] bundlePath];
+    NSString *camlistoredPath = [bundlePath stringByAppendingPathComponent:@"Contents/Resources/camlistored"];
+    
+    // plist for launchctl, don't open the browser
+    NSDictionary *plist = @{
+                            @"Label": @"org.camlistore.plist",
+                            @"ProgramArguments": @[camlistoredPath, @"--openbrowser=false"],
+                            @"RunAtLoad": @YES,
+                            @"KeepAlive": @YES
+                            };
+    
+    [[NSFileManager defaultManager] createDirectoryAtPath:[self supportDirectory] withIntermediateDirectories:YES attributes:nil error:nil];
+    [plist writeToFile:launchPlist atomically:YES];
+    
+    NSTask *task = [[NSTask alloc] init];
+    [task setLaunchPath:@"/bin/launchctl"];
+    [task setCurrentDirectoryPath:[self supportDirectory]];
+    
+    NSArray *args = @[@"load", plistFile];
+    [task setArguments:args];
+    
+    [task launch];
+}
+
+// unload when we quit, this'll stop camlistored
+- (void)launchctlUnload
+{
+    NSTask *task = [[NSTask alloc] init];
+    [task setLaunchPath:@"/bin/launchctl"];
+    [task setCurrentDirectoryPath:[self supportDirectory]];
+    
+    NSArray *args = @[@"unload", plistFile];
+    [task setArguments:args];
+    
+    [task launch];
+}
+
+#pragma mark - support directory
+
+- (NSString *)supportDirectory
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES);
+    NSString *applicationSupportDirectory = [paths objectAtIndex:0];
+    
+    return [applicationSupportDirectory stringByAppendingPathComponent:@"camlistore"];
 }
 
 #pragma mark - kvo for selection
@@ -67,8 +132,6 @@ void *kContextActivePanel = &kContextActivePanel;
 {
     return self.menubarController.statusItemView;
 }
-
-
 
 - (void)dealloc
 {
