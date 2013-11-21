perkeep/perkeep

commit cb31dd58d2c5513e2568e241174d0bf7fbdbf0cc

In-memory search: include FileInfo. More tests pass.
Change-Id: Ic33357b63d1d57dc174fe18360e920730016c903

pkg/index/corpus.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
 	"testing"
 
 	"camlistore.org/pkg/blob"
+	"camlistore.org/pkg/strutil"
 	"camlistore.org/pkg/types/camtypes"
 )
 
@@ -30,17 +32,14 @@ type Corpus struct {
 
 	// TODO: add GoLLRB to third_party; keep sorted BlobMeta
 	keyId      map[blob.Ref]string
-	files      map[blob.Ref]FileMeta
+	files      map[blob.Ref]camtypes.FileInfo
 	permanodes map[blob.Ref]*PermanodeMeta
 
 	// TOOD: use deletedCache instead?
 	deletedBy map[blob.Ref]blob.Ref // key is deleted by value
-}
 
-type FileMeta struct {
-	size      int64
-	mimeType  string
-	wholeRefs []blob.Ref
+	// scratch string slice
+	ss []string
 }
 
 type PermanodeMeta struct {
@@ -52,7 +51,7 @@ func newCorpus() *Corpus {
 	return &Corpus{
 		blobs:      make(map[blob.Ref]*camtypes.BlobMeta),
 		camBlobs:   make(map[string]map[blob.Ref]*camtypes.BlobMeta),
-		files:      make(map[blob.Ref]FileMeta),
+		files:      make(map[blob.Ref]camtypes.FileInfo),
 		permanodes: make(map[blob.Ref]*PermanodeMeta),
 		deletedBy:  make(map[blob.Ref]blob.Ref),
 		keyId:      make(map[blob.Ref]string),
@@ -94,8 +93,23 @@ func (s crashStorage) Find(key string) Iterator {
 
 // *********** Updating the corpus
 
+var corpusMergeFunc = map[string]func(c *Corpus, k, v string) error{
+	"have":        nil, // redundant with "meta"
+	"meta":        (*Corpus).mergeMetaRow,
+	"signerkeyid": (*Corpus).mergeSignerKeyIdRow,
+	"claim":       (*Corpus).mergeClaimRow,
+	"fileinfo":    (*Corpus).mergeFileInfoRow,
+	"filetimes":   (*Corpus).mergeFileTimesRow,
+}
+
 func (c *Corpus) scanFromStorage(s Storage) error {
-	for _, prefix := range []string{"meta:", "signerkeyid:", "claim|"} {
+	for _, prefix := range []string{
+		"meta:",
+		"signerkeyid:",
+		"claim|",
+		"fileinfo|",
+		"filetimes|",
+	} {
 		if err := c.scanPrefix(s, prefix); err != nil {
 			return err
 		}
@@ -116,13 +130,6 @@ func (c *Corpus) scanPrefix(s Storage, prefix string) (err error) {
 		}
 	}
 	return nil
-}
-
-var corpusMergeFunc = map[string]func(c *Corpus, k, v string) error{
-	"have":        nil, // redundant with "meta"
-	"meta":        (*Corpus).mergeMetaRow,
-	"signerkeyid": (*Corpus).mergeSignerKeyIdRow,
-	"claim":       (*Corpus).mergeClaimRow,
 }
 
 func (c *Corpus) addBlob(br blob.Ref, mm mutationMap) error {
@@ -188,6 +195,59 @@ func (c *Corpus) mergeClaimRow(k, v string) error {
 	}
 	pm.Claims = append(pm.Claims, cl)
 	return nil
+}
+
+func (c *Corpus) mergeFileInfoRow(k, v string) error {
+	// fileinfo|sha1-579f7f246bd420d486ddeb0dadbb256cfaf8bf6b" "5|some-stuff.txt|"
+	c.ss = strutil.AppendSplitN(c.ss[:0], k, "|", 2)
+	if len(c.ss) != 2 {
+		return fmt.Errorf("unexpected fileinfo key %q", k)
+	}
+	br, ok := blob.Parse(c.ss[1])
+	if !ok {
+		return fmt.Errorf("unexpected fileinfo blobref in key %q", k)
+	}
+	c.ss = strutil.AppendSplitN(c.ss[:0], v, "|", 3)
+	if len(c.ss) != 3 {
+		return fmt.Errorf("unexpected fileinfo value %q", k)
+	}
+	size, err := strconv.ParseInt(c.ss[0], 10, 64)
+	if err != nil {
+		return fmt.Errorf("unexpected fileinfo value %q", k)
+	}
+	c.mutateFileInfo(br, func(fi *camtypes.FileInfo) {
+		fi.Size = size
+		fi.FileName = c.str(c.ss[1])
+		fi.MIMEType = c.str(c.ss[2])
+	})
+	return nil
+}
+
+func (c *Corpus) mergeFileTimesRow(k, v string) error {
+	if v == "" {
+		return nil
+	}
+	// "filetimes|sha1-579f7f246bd420d486ddeb0dadbb256cfaf8bf6b" "1970-01-01T00%3A02%3A03Z"
+	c.ss = strutil.AppendSplitN(c.ss[:0], k, "|", 2)
+	if len(c.ss) != 2 {
+		return fmt.Errorf("unexpected filetimes key %q", k)
+	}
+	br, ok := blob.Parse(c.ss[1])
+	if !ok {
+		return fmt.Errorf("unexpected filetimes blobref in key %q", k)
+	}
+	c.ss = strutil.AppendSplitN(c.ss[:0], v, ",", -1)
+	times := c.ss
+	c.mutateFileInfo(br, func(fi *camtypes.FileInfo) {
+		updateFileInfoTimes(fi, times)
+	})
+	return nil
+}
+
+func (c *Corpus) mutateFileInfo(br blob.Ref, fn func(*camtypes.FileInfo)) {
+	fi := c.files[br] // use zero value if not present
+	fn(&fi)
+	c.files[br] = fi
 }
 
 // str returns s, interned.
@@ -294,4 +354,14 @@ func (c *Corpus) AppendClaims(dst []camtypes.Claim, permaNode blob.Ref,
 		dst = append(dst, cl)
 	}
 	return dst, nil
+}
+
+func (c *Corpus) GetFileInfo(fileRef blob.Ref) (fi camtypes.FileInfo, err error) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	fi, ok := c.files[fileRef]
+	if !ok {
+		err = os.ErrNotExist
+	}
+	return
 }

pkg/search/query_test.go
@@ -425,7 +425,6 @@ func TestQueryPermanodeAttrValueMatches(t *testing.T) {
 // find permanodes matching a certain file query
 func TestQueryFileConstraint(t *testing.T) { testQuery(t, testQueryFileConstraint, indexClassic) }
 func TestQueryFileConstraint_Scan(t *testing.T) {
-	t.Skip("TODO: fileinfo in memory")
 	testQuery(t, testQueryFileConstraint, indexCorpusScan)
 }
 func TestQueryFileConstraint_Build(t *testing.T) {

pkg/index/index.go
@@ -844,6 +844,9 @@ func (x *Index) loadKey(key string, val *string, err *error, wg *sync.WaitGroup)
 }
 
 func (x *Index) GetFileInfo(fileRef blob.Ref) (camtypes.FileInfo, error) {
+	if x.corpus != nil {
+		return x.corpus.GetFileInfo(fileRef)
+	}
 	ikey := "fileinfo|" + fileRef.String()
 	tkey := "filetimes|" + fileRef.String()
 	wg := new(sync.WaitGroup)
@@ -885,13 +888,20 @@ func (x *Index) GetFileInfo(fileRef blob.Ref) (camtypes.FileInfo, error) {
 
 	if tv != "" {
 		times := strings.Split(urld(tv), ",")
-		fi.Time = types.ParseTime3339OrZil(times[0])
-		if len(times) == 2 {
-			fi.ModTime = types.ParseTime3339OrZil(times[1])
-		}
+		updateFileInfoTimes(&fi, times)
 	}
 
 	return fi, nil
+}
+
+func updateFileInfoTimes(fi *camtypes.FileInfo, times []string) {
+	if len(times) == 0 {
+		return
+	}
+	fi.Time = types.ParseTime3339OrZil(times[0])
+	if len(times) == 2 {
+		fi.ModTime = types.ParseTime3339OrZil(times[1])
+	}
 }
 
 func (x *Index) GetImageInfo(fileRef blob.Ref) (camtypes.ImageInfo, error) {

commit c5037b76e622ebd58cf39da2b2f439a32f6de809

Fix thumbnails, broken in cb31dd58d2c5513e2568e241174d0bf7fbdbf0cc
Change-Id: I82defd54f53d4f6c1a594185d5e9b7d3a4a92c07

pkg/index/corpus.go
@@ -217,8 +217,8 @@ func (c *Corpus) mergeFileInfoRow(k, v string) error {
 	}
 	c.mutateFileInfo(br, func(fi *camtypes.FileInfo) {
 		fi.Size = size
-		fi.FileName = c.str(c.ss[1])
-		fi.MIMEType = c.str(c.ss[2])
+		fi.FileName = c.str(urld(c.ss[1]))
+		fi.MIMEType = c.str(urld(c.ss[2]))
 	})
 	return nil
 }

pkg/index/export_test.go
@@ -16,10 +16,27 @@ limitations under the License.
 
 package index
 
+import (
+	"camlistore.org/pkg/blob"
+	"camlistore.org/pkg/types/camtypes"
+)
+
 func ExpReverseTimeString(s string) string {
 	return reverseTimeString(s)
 }
 
 func ExpUnreverseTimeString(s string) string {
 	return unreverseTimeString(s)
+}
+
+func ExpNewCorpus() *Corpus {
+	return newCorpus()
+}
+
+func (c *Corpus) Exp_mergeFileInfoRow(k, v string) error {
+	return c.mergeFileInfoRow(k, v)
+}
+
+func (c *Corpus) Exp_files(br blob.Ref) camtypes.FileInfo {
+	return c.files[br]
 }

pkg/index/index_test.go
@@ -23,11 +23,14 @@ import (
 	"go/token"
 	"os"
 	"path/filepath"
+	"reflect"
 	"strings"
 	"testing"
 
+	"camlistore.org/pkg/blob"
 	"camlistore.org/pkg/index"
 	"camlistore.org/pkg/index/indextest"
+	"camlistore.org/pkg/types/camtypes"
 )
 
 func TestReverseTimeString(t *testing.T) {
@@ -161,4 +164,19 @@ func skipDir(name string) bool {
 		}
 	}
 	return false
+}
+
+func TestMergeFileInfoRow(t *testing.T) {
+	c := index.ExpNewCorpus()
+	c.Exp_mergeFileInfoRow("fileinfo|sha1-579f7f246bd420d486ddeb0dadbb256cfaf8bf6b",
+		"100|something%2egif|image%2Fgif")
+	fi := c.Exp_files(blob.MustParse("sha1-579f7f246bd420d486ddeb0dadbb256cfaf8bf6b"))
+	want := camtypes.FileInfo{
+		Size:     100,
+		MIMEType: "image/gif",
+		FileName: "something.gif",
+	}
+	if !reflect.DeepEqual(want, fi) {
+		t.Errorf("Got %+v; want %+v", fi, want)
+	}
 }

pkg/search/query_test.go
@@ -470,3 +470,35 @@ func TestQueryPermanodeModtime(t *testing.T) {
 		qt.wantRes(sq, p2)
 	})
 }
+
+// This really belongs in pkg/index for the index-vs-corpus tests, but
+// it's easier here for now.
+// TODO: make all the indextest/tests.go
+// also test the three memory build modes that testQuery does.
+func TestDecodeFileInfo(t *testing.T) {
+	t.Skip("TODO: finish; panics now on imageinfo calls")
+	testQuery(t, func(qt *queryTest) {
+		id := qt.id
+		fileRef, _ := id.UploadFile("file.gif", "GIF87afoo", time.Unix(456, 0))
+		res, err := qt.Handler().Describe(&DescribeRequest{
+			BlobRef: fileRef,
+		})
+		if err != nil {
+			qt.t.Error(err)
+			return
+		}
+		db := res.Meta[fileRef.String()]
+		if db == nil {
+			qt.t.Error("DescribedBlob missing")
+			return
+		}
+		if db.File == nil {
+			qt.t.Error("DescribedBlob.File is nil")
+			return
+		}
+		if db.File.MIMEType != "image/gif" {
+			qt.t.Error("DescribedBlob.File = %+v; mime type is not image/gif", db.File)
+			return
+		}
+	})
+}