fc1dfc71a22244e53919aeeb1dcd13eceb4db138
Brad Fitzpatrick <brad@danga.com>
2013-12-12 09:08:49 +0400

    syncutil: more RWMutexTracker logging and tracking
    Change-Id: I02032a49b7413f56f8283b4ab446fcb0474d4ff3

--- pkg/syncutil/lock.go
+++ pkg/syncutil/lock.go
@@ -20,6 +20,8 @@ import (
 	"log"
 	"runtime"
 	"sync"
+	"sync/atomic"
+	"time"
 )
 
 // RWMutexTracker is a sync.RWMutex that tracks who owns the current
@@ -27,14 +29,40 @@ import (
 type RWMutexTracker struct {
 	mu sync.RWMutex
 
+	// Atomic counters for number waiting and having read and write locks.
+	nwaitr int32
+	nwaitw int32
+	nhaver int32
+	nhavew int32 // should always be 0 or 1
+
+	logOnce sync.Once
+
 	hmu    sync.Mutex
 	holder []byte
 }
 
 const stackBufSize = 16 << 20
 
+func (m *RWMutexTracker) startLogger() {
+	go func() {
+		for {
+			time.Sleep(1 * time.Second)
+			log.Printf("Mutex %p: waitW %d haveW %d   waitR %d haveR %d",
+				m,
+				atomic.LoadInt32(&m.nwaitw),
+				atomic.LoadInt32(&m.nhavew),
+				atomic.LoadInt32(&m.nwaitr),
+				atomic.LoadInt32(&m.nhaver))
+		}
+	}()
+}
+
 func (m *RWMutexTracker) Lock() {
+	m.logOnce.Do(m.startLogger)
+	atomic.AddInt32(&m.nwaitw, 1)
 	m.mu.Lock()
+	atomic.AddInt32(&m.nwaitw, -1)
+	atomic.AddInt32(&m.nhavew, 1)
 
 	m.hmu.Lock()
 	if len(m.holder) == 0 {
@@ -47,13 +75,27 @@ func (m *RWMutexTracker) Lock() {
 
 func (m *RWMutexTracker) Unlock() {
 	m.hmu.Lock()
-	m.holder = m.holder[:0]
+	m.holder = m.holder[:runtime.Stack(m.holder[:stackBufSize], false)]
+	log.Printf("Unlock at %s", m.holder)
+
 	m.hmu.Unlock()
+
+	atomic.AddInt32(&m.nhavew, -1)
 	m.mu.Unlock()
 }
 
-func (m *RWMutexTracker) RLock()   { m.mu.RLock() }
-func (m *RWMutexTracker) RUnlock() { m.mu.RUnlock() }
+func (m *RWMutexTracker) RLock() {
+	m.logOnce.Do(m.startLogger)
+	atomic.AddInt32(&m.nwaitr, 1)
+	m.mu.RLock()
+	atomic.AddInt32(&m.nwaitr, -1)
+	atomic.AddInt32(&m.nhaver, 1)
+}
+
+func (m *RWMutexTracker) RUnlock() {
+	atomic.AddInt32(&m.nhaver, -1)
+	m.mu.RUnlock()
+}
 
 // Holder returns the stack trace of the current exclusive lock holder's stack
 // when it acquired the lock (with Lock). It returns the empty string if the lock
