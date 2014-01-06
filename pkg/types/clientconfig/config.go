d21aa1dba20befdd66a856ccdd10f5e0702e2435
Brad Fitzpatrick <brad@danga.com>
2014-01-06 08:05:12 -0800

    year++
    Change-Id: Ib5e17c39d3178357e1c18c4407ad45e91170ef16

--- pkg/types/clientconfig/config.go
+++ pkg/types/clientconfig/config.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2013 The Camlistore Authors.
+Copyright 2014 The Camlistore Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
