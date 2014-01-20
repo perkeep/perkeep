2c03760157d3e2f3a61fd78921065113aef7f08e
Daniel Erat <dan@erat.org>
2014-01-19 18:53:44 -0800

    third_party/taglib: Update to ef5ded85df.
    Change-Id: I536e08d2c0184b1d2abe52f3f139be35e9f331dd

--- third_party/taglib/taglib.go
+++ third_party/taglib/taglib.go
@@ -1,3 +1,17 @@
+// Copyright 2013 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package taglib provides utilities for parsing audio tags in
 // various formats.
 package taglib
@@ -26,6 +40,16 @@ type GenericTag interface {
 	Genre() string
 	Year() time.Time
 	Track() uint32
+	Disc() uint32
+
+	// CustomFrames returns non-standard, user-defined frames as a map from
+	// descriptions (e.g. "PERFORMER", "MusicBrainz Album Id", etc.) to
+	// values.
+	CustomFrames() map[string]string
+
+	// TagSize returns the total size of the tag's header and frames,
+	// i.e. the position at which audio data starts.
+	TagSize() uint32
 }
 
 // Decode reads r and determines which tag format the data is in, if
--- third_party/taglib/taglib_test.go
+++ third_party/taglib/taglib_test.go
@@ -1,3 +1,17 @@
+// Copyright 2013 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package taglib
 
 import (
@@ -11,8 +25,11 @@ func ExampleDecode() {
 	if err != nil {
 		panic(err)
 	}
-
-	tag, err := Decode(f)
+	fi, err := f.Stat()
+	if err != nil {
+		panic(err)
+	}
+	tag, err := Decode(f, fi.Size())
 	if err != nil {
 		panic(err)
 	}
@@ -22,7 +39,9 @@ func ExampleDecode() {
 	fmt.Println("Album:", tag.Album())
 	fmt.Println("Genre:", tag.Genre())
 	fmt.Println("Year:", tag.Year())
+	fmt.Println("Disc:", tag.Disc())
 	fmt.Println("Track:", tag.Track())
+	fmt.Println("Performer:", tag.CustomFrames()["PERFORMER"])
 
 	// Output:
 	// Title: Test Name
@@ -30,5 +49,7 @@ func ExampleDecode() {
 	// Album: Test Album
 	// Genre: Classical
 	// Year: 2008-01-01 00:00:00 +0000 UTC
+	// Disc: 3
 	// Track: 7
+	// Performer: Somebody
 }
