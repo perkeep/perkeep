1e56abec9085bfbba5bb0fc2d6b8c1a7d5c15fe7
Brad Fitzpatrick <brad@danga.com>
2014-02-08 17:07:56 -0800

    Android: build in Docker, part 2. Build the Go binary child process too.
    Now uploaded to the Google Play App Store Market.
    
    Change-Id: I05f94eaf3ba70d694c593a892c15280364acf4ee

---
+++ clients/android/build-in-docker.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+
+use strict;
+use File::Path qw(make_path);
+
+die "This script is meant to be run within the camlistore/android Docker contain. Run 'make env' to build it.\n"
+    unless $ENV{IN_DOCKER};
+
+my $mode = shift || "debug";
+
+my $ANDROID = "/src/camlistore.org/clients/android";
+my $ASSETS = "$ANDROID/assets";
+my $GENDIR = "$ANDROID/gen/org/camlistore";
+
+umask 0;
+make_path($GENDIR, { mode => 0755 }) unless -d $GENDIR;
+
+$ENV{GOROOT} = "/usr/local/go";
+$ENV{GOBIN} = $GENDIR;
+$ENV{GOPATH} = "/";
+$ENV{GOARCH} = "arm";
+print "Building ARM camlistore.org/cmd/camput\n";
+system("/usr/local/go/bin/go", "install", "camlistore.org/cmd/camput")
+    and die "Failed to build camput";
+
+system("cp", "-p", "$GENDIR/linux_arm/camput", "$ASSETS/camput.arm")
+    and die "cp failure";
+# TODO: build an x86 version too? if/when those Android devices matter.
+
+chdir $ASSETS or  die "can't cd to assets dir";
+
+my $digest = `openssl sha1 camput.arm`;
+chomp $digest;
+print "ARM camput is $digest\n";
+die "No digest" unless $digest;
+write_file("$GENDIR/ChildProcessConfig.java", "package org.camlistore; public final class ChildProcessConfig { // $digest\n}");
+
+print "Running ant $mode\n";
+chdir $ANDROID or die "can't cd to android dir";
+exec "ant",
+    "-Dsdk.dir=/usr/local/android-sdk-linux",
+    "-Dkey.store=/keys/android-camlistore.keystore",
+    "-Dkey.alias=camkey",
+    $mode;
+
+sub write_file {
+    my ($file, $contents) = @_;
+    if (open(my $fh, $file)) {
+        my $cur = do { local $/; <$fh> };
+        return if $cur eq $contents;
+    }
+    open(my $fh, ">$file") or die "Failed to open $file: $!";
+    print $fh $contents;
+    close($fh) or die "Close: $!";
+    print "Wrote $file\n";
+}
