dfd94e505d4d8624ec37ccac83e578a2dc6e9835
Brad Fitzpatrick <brad@danga.com>
2014-02-19 00:07:28 -0800

    camput: start of a deadlock test
    Currently disabled until it's working reliably. See comments in code.
    
    Much of this is from Tamás Gulácsi in https://camlistore-review.googlesource.com/#/c/2128/
    
    Change-Id: I2fe2abfdee98584c698a2ac52202f13dda9beef2

--- cmd/camput/camput_test.go
+++ cmd/camput/camput_test.go
@@ -19,8 +19,11 @@ package main
 import (
 	"bytes"
 	"io"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"runtime"
+	"strconv"
 	"testing"
 	"time"
 
@@ -92,4 +95,80 @@ func TestUploadingChangingDirectory(t *testing.T) {
 	//    $ ./dev-camput  -file /tmp/somedir/ 2>&1 | tee /tmp/somedir/log
 	// ... verify it doesn't hang.
 	t.Logf("TODO")
+}
+
+// Tests that uploads of deep directory trees don't deadlock.
+// See commit ee4550bff453526ebae460da1ad59f6e7f3efe77 for backstory
+func TestUploadDirectories(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
+	if v, _ := strconv.ParseBool(os.Getenv("RUN_UPLOAD_DEADLOCK_TEST")); !v {
+		// Temporary. For now the test isn't working (failing) reliably.
+		// Once the test fails reliably, then we fix.
+		t.Skip("skipping test without RUN_UPLOAD_DEADLOCK_TEST=1 in environment")
+	}
+
+	debugFlagOnce.Do(registerDebugFlags)
+
+	baseDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Errorf("error creating temp dir: %v", err)
+		return
+	}
+	defer os.RemoveAll(baseDir)
+
+	uploadRoot := filepath.Join(baseDir, "to_upload")
+	mustMkdir(t, uploadRoot, 0700)
+	// TODO: make wider trees under uploadRoot, taking care to
+	// name dirs and files such that alphabetic statting causes a
+	// deadlock.  This isn't sufficient yet:
+	dirIter := baseDir
+	for i := 0; i < 10; i++ {
+		dirPath := filepath.Join(dirIter, "dir")
+		mustMkdir(t, dirPath, 0700)
+		for _, baseFile := range []string{"file", "FILE.txt"} {
+			filePath := filepath.Join(dirPath, baseFile)
+			if err := ioutil.WriteFile(filePath, []byte("some file contents "+filePath), 0600); err != nil {
+				t.Fatalf("error writing to %s: %v", filePath, err)
+			}
+			t.Logf("Wrote file %s", filePath)
+		}
+		dirIter = dirPath
+	}
+	firstDir := filepath.Join(baseDir, "dir")
+	t.Logf("Will upload %s", firstDir)
+
+	defer setAndRestore(&uploadWorkers, 1)()
+
+	e := &env{
+		Timeout: 5 * time.Second,
+	}
+	stdout, stderr, exit := e.Run(
+		"--blobdir="+baseDir,
+		"--havecache=false",
+		"--verbose",
+		"file",
+		firstDir)
+	if exit != 0 {
+		t.Fatalf("Exit status %d: stdout=[%s], stderr=[%s]", exit, stdout, stderr)
+	}
+}
+
+func mustMkdir(t *testing.T, fn string, mode int) {
+	if err := os.Mkdir(fn, 0700); err != nil {
+		t.Errorf("error creating dir %s: %v", fn, err)
+	}
+}
+
+func setAndRestore(dst *int, v int) func() {
+	old := *dst
+	*dst = v
+	return func() { *dst = old }
+}
+
+func setAndRestoreBool(dst *bool, v bool) func() {
+	old := *dst
+	*dst = v
+	return func() { *dst = old }
 }
