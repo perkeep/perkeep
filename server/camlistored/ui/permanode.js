01f2173e645cfb396748332abfca7a4a585be966
Brad Fitzpatrick <brad@danga.com>
2014-02-27 21:06:03 -0800

    ui: fix link from old detail page to new UI detail page
    Change-Id: Iaa36f532169d3f6697060d9baa2178caaa19423d

--- server/camlistored/ui/permanode.js
+++ server/camlistored/ui/permanode.js
@@ -64,7 +64,7 @@ cam.PermanodePage.prototype.enterDocument = function() {
 	if (permanode) {
 		goog.dom.getElement('permanode').innerHTML = "<a href='./?p=" + permanode + "'>" + permanode + "</a>";
 		goog.dom.getElement('permanodeBlob').innerHTML = "<a href='./?b=" + permanode + "'>view blob</a>";
-		goog.dom.getElement('detailLink').innerHTML = "<a href='./detail.html?" + permanode + "'>new hotness</a>"
+		goog.dom.getElement('detailLink').innerHTML = "<a href='./?p=" + permanode + "&newui=1'>new hotness</a>"
 	}
 
 	// TODO(mpl): use this.eh_ instead?
