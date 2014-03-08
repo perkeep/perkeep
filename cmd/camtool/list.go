perkeep/perkeep

commit 08923e1c00a52a0d9caa7693b375b42789fed052

camtool sync: warn when src and dest have different identities
http://camlistore.org/issue/281

Change-Id: I2e2d595534b14476293a031b88e312de3e0b3f4a

cmd/camtool/sync.go
@@ -35,9 +35,11 @@ import (
 )
 
 type syncCmd struct {
-	src   string
-	dest  string
-	third string
+	src       string
+	dest      string
+	third     string
+	srcKeyID  string // GPG public key ID of the source server, if supported.
+	destKeyID string // GPG public key ID of the destination server, if supported.
 
 	loop        bool
 	verbose     bool
@@ -75,7 +77,7 @@ func (c *syncCmd) Describe() string {
 }
 
 func (c *syncCmd) Usage() {
-	fmt.Fprintf(os.Stderr, "Usage: camtool [globalopts] sync [syncopts] \n")
+	fmt.Fprintf(cmdmain.Stderr, "Usage: camtool [globalopts] sync [syncopts] \n")
 }
 
 func (c *syncCmd) Examples() []string {
@@ -90,7 +92,7 @@ func (c *syncCmd) RunCommand(args []string) error {
 		return cmdmain.UsageError("Can't use --loop without --removesrc")
 	}
 	if c.verbose {
-		c.logger = log.New(os.Stderr, "", 0) // else nil
+		c.logger = log.New(cmdmain.Stderr, "", 0) // else nil
 	}
 	if c.all {
 		err := c.syncAll()
@@ -111,6 +113,15 @@ func (c *syncCmd) RunCommand(args []string) error {
 	ts, err := c.storageFromParam("thirdleg", c.third)
 	if err != nil {
 		return err
+	}
+
+	differentKeyIDs := fmt.Sprintf("WARNING: the source server GPG key ID (%v) and the destination's (%v) differ. All blobs will be synced, but because the indexer at the other side is indexing claims by a different user, you may not see what you expect in that server's web UI, etc.", c.srcKeyID, c.destKeyID)
+
+	if c.srcKeyID != c.destKeyID { // both blank is ok.
+		// Warn at the top (and hope the user sees it and can abort if it was a mistake):
+		fmt.Fprintln(cmdmain.Stderr, differentKeyIDs)
+		// Warn also at the end (in case the user missed the first one)
+		defer fmt.Fprintln(cmdmain.Stderr, differentKeyIDs)
 	}
 
 	passNum := 0
@@ -183,6 +194,16 @@ func (c *syncCmd) storageFromParam(which storageType, val string) (blobserver.St
 		return nil, fmt.Errorf("could not setup auth for connecting to %v: %v", val, err)
 	}
 	cl.SetLogger(c.logger)
+	serverKeyID, err := cl.ServerKeyID()
+	if err != nil && err != client.ErrNoSigning {
+		fmt.Fprintf(cmdmain.Stderr, "Failed to discover keyId for server %v: %v", val, err)
+	} else {
+		if which == storageSource {
+			c.srcKeyID = serverKeyID
+		} else if which == storageDest {
+			c.destKeyID = serverKeyID
+		}
+	}
 	return cl, nil
 }
 
@@ -304,7 +325,7 @@ func (c *syncCmd) doPass(src, dest, thirdLeg blobserver.Storage) (stats SyncStat
 
 	if c.dest == "stdout" {
 		for sb := range srcBlobs {
-			fmt.Printf("%s %d\n", sb.Ref, sb.Size)
+			fmt.Fprintf(cmdmain.Stderr, "%s %d\n", sb.Ref, sb.Size)
 		}
 		checkSourceError()
 		return
@@ -364,7 +385,7 @@ func (c *syncCmd) doPass(src, dest, thirdLeg blobserver.Storage) (stats SyncStat
 	}
 
 	for sb := range syncBlobs {
-		fmt.Printf("Destination needs blob: %s\n", sb)
+		fmt.Fprintf(cmdmain.Stderr, "Destination needs blob: %s\n", sb)
 
 		blobReader, size, err := src.FetchStreaming(sb.Ref)
 		if err != nil {

pkg/client/client.go
@@ -67,6 +67,7 @@ type Client struct {
 	downloadHelper string      // or "" if none
 	storageGen     string      // storage generation, or "" if not reported
 	syncHandlers   []*SyncInfo // "from" and "to" url prefix for each syncHandler
+	serverKeyID    string      // Server's GPG public key ID.
 
 	signerOnce sync.Once
 	signer     *schema.Signer
@@ -345,6 +346,9 @@ func (c *Client) Stats() Stats {
 // ErrNoSearchRoot is returned by SearchRoot if the server doesn't support search.
 var ErrNoSearchRoot = errors.New("client: server doesn't support search")
 
+// ErrNoSigning is returned by ServerKeyID if the server doesn't support signing.
+var ErrNoSigning = fmt.Errorf("client: server doesn't support signing")
+
 // ErrNoStorageGeneration is returned by StorageGeneration if the
 // server doesn't report a storage generation value.
 var ErrNoStorageGeneration = errors.New("client: server doesn't report a storage generation")
@@ -362,6 +366,18 @@ func (c *Client) BlobRoot() (string, error) {
 		return "", err
 	}
 	return prefix + "/", nil
+}
+
+// ServerKeyID returns the server's GPG public key ID.
+// If the server isn't running a sign handler, the error will be ErrNoSigning.
+func (c *Client) ServerKeyID() (string, error) {
+	if err := c.condDiscovery(); err != nil {
+		return "", err
+	}
+	if c.serverKeyID == "" {
+		return "", ErrNoSigning
+	}
+	return c.serverKeyID, nil
 }
 
 // SearchRoot returns the server's search handler.
@@ -756,6 +772,10 @@ func (c *Client) doDiscovery() error {
 				ToIndex: toIndex,
 			})
 		}
+	}
+	serverSigning, ok := m["signing"].(map[string]interface{})
+	if ok {
+		c.serverKeyID = serverSigning["publicKeyId"].(string)
 	}
 	return nil
 }

pkg/server/ui.go
@@ -455,6 +455,8 @@ func (ui *UIHandler) populateDiscoveryMap(m map[string]interface{}) {
 		"directoryHelper": path.Join(ui.prefix, "tree") + "/",
 		"publishRoots":    pubRoots,
 	}
+	// TODO(mpl): decouple discovery of the sig handler from the
+	// existence of a ui handler.
 	if ui.sigh != nil {
 		uiDisco["signing"] = ui.sigh.DiscoveryMap(ui.JSONSignRoot)
 	}

commit 73526917a1f5b05a40c46da59fc0c4a75423b594

camtool list/sync: fix 3 bugs
1) revert to printing to stdout (broke in
08923e1c00a52a0d9caa7693b375b42789fed052) for actual output

2) do not warn about different identities when "syncing" to stdout

3) check blob was actually described before using its fields

Change-Id: Ibb3aa4c560e1b0036aba080807400e8820163cc9

cmd/camtool/list.go
@@ -71,14 +71,14 @@ func (c *listCmd) RunCommand(args []string) error {
 		return c.syncCmd.RunCommand(args)
 	}
 
-	stdout := os.Stdout
-	defer func() { os.Stdout = stdout }()
+	stdout := cmdmain.Stdout
+	defer func() { cmdmain.Stdout = stdout }()
 	pr, pw, err := os.Pipe()
 	if err != nil {
 		return fmt.Errorf("Could not create pipe to read from stdout: %v", err)
 	}
 	defer pr.Close()
-	os.Stdout = pw
+	cmdmain.Stdout = pw
 
 	if err := c.setClient(); err != nil {
 		return err
@@ -107,7 +107,12 @@ func (c *listCmd) RunCommand(args []string) error {
 			return fmt.Errorf("Error when describing blobs %v: %v", blobRefs, err)
 		}
 		for _, v := range blobRefs {
-			blob := described.Meta[v.String()]
+			blob, ok := described.Meta[v.String()]
+			if !ok {
+				// This can happen if the index is out of sync with the storage we enum from.
+				fmt.Fprintf(stdout, "%v <not described>\n", v)
+				continue
+			}
 			detailed := detail(blob)
 			if detailed != "" {
 				detailed = fmt.Sprintf("\t%v", detailed)

cmd/camtool/sync.go
@@ -117,7 +117,7 @@ func (c *syncCmd) RunCommand(args []string) error {
 
 	differentKeyIDs := fmt.Sprintf("WARNING: the source server GPG key ID (%v) and the destination's (%v) differ. All blobs will be synced, but because the indexer at the other side is indexing claims by a different user, you may not see what you expect in that server's web UI, etc.", c.srcKeyID, c.destKeyID)
 
-	if c.srcKeyID != c.destKeyID { // both blank is ok.
+	if c.dest != "stdout" && c.srcKeyID != c.destKeyID { // both blank is ok.
 		// Warn at the top (and hope the user sees it and can abort if it was a mistake):
 		fmt.Fprintln(cmdmain.Stderr, differentKeyIDs)
 		// Warn also at the end (in case the user missed the first one)
@@ -325,7 +325,7 @@ func (c *syncCmd) doPass(src, dest, thirdLeg blobserver.Storage) (stats SyncStat
 
 	if c.dest == "stdout" {
 		for sb := range srcBlobs {
-			fmt.Fprintf(cmdmain.Stderr, "%s %d\n", sb.Ref, sb.Size)
+			fmt.Fprintf(cmdmain.Stdout, "%s %d\n", sb.Ref, sb.Size)
 		}
 		checkSourceError()
 		return
@@ -385,7 +385,7 @@ func (c *syncCmd) doPass(src, dest, thirdLeg blobserver.Storage) (stats SyncStat
 	}
 
 	for sb := range syncBlobs {
-		fmt.Fprintf(cmdmain.Stderr, "Destination needs blob: %s\n", sb)
+		fmt.Fprintf(cmdmain.Stdout, "Destination needs blob: %s\n", sb)
 
 		blobReader, size, err := src.FetchStreaming(sb.Ref)
 		if err != nil {