db1a4cf6aab2edbf73e23c3788068914b4ebe842
Steven L. Speek <slspeek@gmail.com>
2014-04-13 23:00:24 +0200

    search: Self-documenting atoms
    Parse errors in the atoms have a position too
    Easier to add new search predicates
    
    Change-Id: I9d2e890b9afcfb524aafa8f1e4e1debc683555ac

--- pkg/search/expr.go
+++ pkg/search/expr.go
@@ -17,36 +17,15 @@ limitations under the License.
 package search
 
 import (
-	"errors"
 	"fmt"
 	"log"
-	"regexp"
 	"strconv"
 	"strings"
-	"time"
 
 	"camlistore.org/pkg/context"
-	"camlistore.org/pkg/geocode"
-	"camlistore.org/pkg/types"
 )
 
 const seeDocs = "\nSee: https://camlistore.googlesource.com/camlistore/+/master/doc/search-ui.txt"
-
-var (
-	tagExpr   = regexp.MustCompile(`^tag:(.+)$`)
-	titleExpr = regexp.MustCompile(`^title:(.+)$`)
-	attrExpr  = regexp.MustCompile(`^attr:(\w+):(.+)$`)
-
-	// childrenof:sha1-xxxx where xxxx is a full blobref or even
-	// just a prefix of one. only matches permanodes currently.
-	childrenOfExpr = regexp.MustCompile(`^childrenof:(\S+)$`)
-
-	// used for width/height ranges. 10 is max length of 32-bit
-	// int (strconv.Atoi on 32-bit platforms), even though a max
-	// JPEG dimension is only 16-bit.
-	whRangeExpr = regexp.MustCompile(`^(\d{0,10})-(\d{0,10})$`)
-	whValueExpr = regexp.MustCompile(`^(\d{0,10})$`)
-)
 
 var (
 	noMatchingOpening      = "No matching opening parenthesis"
@@ -272,8 +251,12 @@ func (p *parser) parseOperand(ctx *context.Context) (c *Constraint, err error) {
 	return
 }
 
-func (p *parser) atomWord() (word string, err error) {
+// AtomWords returns the parsed atom, the starting position of this
+// atom and an error.
+func (p *parser) atomWords() (a atom, start int, err error) {
 	i := p.peek()
+	start = i.start
+	a = atom{}
 	switch i.typ {
 	case tokenLiteral:
 		err = newParseExpError(noLiteralSupport, *i)
@@ -286,261 +269,57 @@ func (p *parser) atomWord() (word string, err error) {
 		return
 	case tokenPredicate:
 		i := p.next()
-		word += i.val
+		a.predicate = i.val
 	}
 	for {
 		switch p.peek().typ {
 		case tokenColon:
 			p.next()
-			word += ":"
 			continue
 		case tokenArg:
 			i := p.next()
-			word += i.val
+			a.args = append(a.args, i.val)
 			continue
 		case tokenQuotedArg:
 			i := p.next()
-			uq, err := strconv.Unquote(i.val)
+			var uq string
+			uq, err = strconv.Unquote(i.val)
 			if err != nil {
-				return "", err
+				return
 			}
-			word += uq
+			a.args = append(a.args, uq)
 			continue
 		}
 		return
 	}
 }
 
-func (p *parser) parseAtom(ctx *context.Context) (c *Constraint, err error) {
-	word, err := p.atomWord()
+func (p *parser) parseAtom(ctx *context.Context) (*Constraint, error) {
+	a, start, err := p.atomWords()
 	if err != nil {
-		return
+		return nil, err
 	}
-	c, err = parseCoreAtom(ctx, word)
-	if err == nil {
-		return c, nil
-	}
-	c, err = parseImageAtom(ctx, word)
-	if err == nil {
-		return c, nil
-	}
-	c, err = parseLocationAtom(ctx, word)
-	if err == nil {
-		return c, nil
-	}
-	log.Printf("Unknown search predicate %q", word)
-	return nil, errors.New(fmt.Sprintf("Unknown search predicate: %q", word))
-}
-
-func permOfFile(fc *FileConstraint) *Constraint {
-	return &Constraint{
-		Permanode: &PermanodeConstraint{
-			Attr:       "camliContent",
-			ValueInSet: &Constraint{File: fc},
-		},
-	}
-}
-
-func whRatio(fc *FloatConstraint) *Constraint {
-	return permOfFile(&FileConstraint{
-		IsImage: true,
-		WHRatio: fc,
-	})
-}
-
-func parseWHExpression(expr string) (min, max string, err error) {
-	if m := whRangeExpr.FindStringSubmatch(expr); m != nil {
-		return m[1], m[2], nil
-	}
-	if m := whValueExpr.FindStringSubmatch(expr); m != nil {
-		return m[1], m[1], nil
-	}
-	return "", "", errors.New("bogus range or value")
-}
-
-func parseImageAtom(ctx *context.Context, word string) (*Constraint, error) {
-	if word == "is:image" {
-		c := &Constraint{
-			Permanode: &PermanodeConstraint{
-				Attr: "camliContent",
-				ValueInSet: &Constraint{
-					File: &FileConstraint{
-						IsImage: true,
-					},
-				},
-			},
+	faultToken := func() token {
+		return token{
+			typ:   tokenError,
+			val:   a.String(),
+			start: start,
 		}
-		return c, nil
 	}
-	if word == "is:landscape" {
-		return whRatio(&FloatConstraint{Min: 1.0}), nil
-	}
-	if word == "is:portrait" {
-		return whRatio(&FloatConstraint{Max: 1.0}), nil
-	}
-	if word == "is:pano" {
-		return whRatio(&FloatConstraint{Min: 2.0}), nil
-	}
-	if strings.HasPrefix(word, "width:") {
-		mins, maxs, err := parseWHExpression(strings.TrimPrefix(word, "width:"))
-		if err != nil {
-			return nil, err
-		}
-		c := permOfFile(&FileConstraint{
-			IsImage: true,
-			Width:   whIntConstraint(mins, maxs),
-		})
-		return c, nil
-	}
-	if strings.HasPrefix(word, "height:") {
-		mins, maxs, err := parseWHExpression(strings.TrimPrefix(word, "height:"))
-		if err != nil {
-			return nil, err
-		}
-		c := permOfFile(&FileConstraint{
-			IsImage: true,
-			Height:  whIntConstraint(mins, maxs),
-		})
-		return c, nil
-	}
-	return nil, errors.New(fmt.Sprintf("Not an image-atom: %v", word))
-}
-
-func parseCoreAtom(ctx *context.Context, word string) (*Constraint, error) {
-	if m := tagExpr.FindStringSubmatch(word); m != nil {
-		c := &Constraint{
-			Permanode: &PermanodeConstraint{
-				Attr:       "tag",
-				SkipHidden: true,
-				Value:      m[1],
-			},
-		}
-		return c, nil
-	}
-	if m := titleExpr.FindStringSubmatch(word); m != nil {
-		c := &Constraint{
-			Permanode: &PermanodeConstraint{
-				Attr:       "title",
-				SkipHidden: true,
-				ValueMatches: &StringConstraint{
-					Contains:        m[1],
-					CaseInsensitive: true,
-				},
-			},
-		}
-		return c, nil
-	}
-	if m := attrExpr.FindStringSubmatch(word); m != nil {
-		c := &Constraint{
-			Permanode: &PermanodeConstraint{
-				Attr:       m[1],
-				SkipHidden: true,
-				Value:      m[2],
-			},
-		}
-		return c, nil
-	}
-	if m := childrenOfExpr.FindStringSubmatch(word); m != nil {
-		c := &Constraint{
-			Permanode: &PermanodeConstraint{
-				Relation: &RelationConstraint{
-					Relation: "parent",
-					Any: &Constraint{
-						BlobRefPrefix: m[1],
-					},
-				},
-			},
-		}
-		return c, nil
-	}
-	if strings.HasPrefix(word, "before:") || strings.HasPrefix(word, "after:") {
-		before := false
-		when := ""
-		if strings.HasPrefix(word, "before:") {
-			before = true
-			when = strings.TrimPrefix(word, "before:")
-		} else {
-			when = strings.TrimPrefix(word, "after:")
-		}
-		base := "0000-01-01T00:00:00Z"
-		if len(when) < len(base) {
-			when += base[len(when):]
-		}
-		t, err := time.Parse(time.RFC3339, when)
-		if err != nil {
-			return nil, err
-		}
-		tc := &TimeConstraint{}
-		if before {
-			tc.Before = types.Time3339(t)
-		} else {
-			tc.After = types.Time3339(t)
-		}
-		c := &Constraint{
-			Permanode: &PermanodeConstraint{
-				Time: tc,
-			},
-		}
-		return c, nil
-	}
-	if strings.HasPrefix(word, "format:") {
-		c := permOfFile(&FileConstraint{
-			MIMEType: &StringConstraint{
-				Equals: mimeFromFormat(strings.TrimPrefix(word, "format:")),
-			},
-		})
-		return c, nil
-	}
-	return nil, errors.New(fmt.Sprintf("Not an core-atom: %v", word))
-}
-
-func parseLocationAtom(ctx *context.Context, word string) (*Constraint, error) {
-	if strings.HasPrefix(word, "loc:") {
-		where := strings.TrimPrefix(word, "loc:")
-		rects, err := geocode.Lookup(ctx, where)
-		if err != nil {
-			return nil, err
-		}
-		if len(rects) == 0 {
-			return nil, fmt.Errorf("No location found for %q", where)
-		}
-		var c *Constraint
-		for i, rect := range rects {
-			loc := &LocationConstraint{
-				West:  rect.SouthWest.Long,
-				East:  rect.NorthEast.Long,
-				North: rect.NorthEast.Lat,
-				South: rect.SouthWest.Lat,
+	var c *Constraint
+	for _, k := range keywords {
+		if k.Match(a) {
+			c, err = k.Predicate(ctx, a.args)
+			if err != nil {
+				return nil, newParseExpError(err.Error(), faultToken())
 			}
-			fileLoc := permOfFile(&FileConstraint{
-				IsImage:  true,
-				Location: loc,
-			})
-			permLoc := &Constraint{
-				Permanode: &PermanodeConstraint{
-					Location: loc,
-				},
-			}
-			rectConstraint := orConst(fileLoc, permLoc)
-			if i == 0 {
-				c = rectConstraint
-			} else {
-				c = orConst(c, rectConstraint)
-			}
+			return c, nil
 		}
-		return c, nil
 	}
-	if word == "has:location" {
-		c := permOfFile(&FileConstraint{
-			IsImage: true,
-			Location: &LocationConstraint{
-				Any: true,
-			},
-		})
-		return c, nil
-	}
-
-	return nil, errors.New(fmt.Sprintf("Not an location-atom: %v", word))
+	t := faultToken()
+	err = newParseExpError(fmt.Sprintf("Unknown search predicate: %q", t.val), t)
+	log.Printf(err.Error())
+	return nil, err
 }
 
 func parseExpression(ctx *context.Context, exp string) (*SearchQuery, error) {
@@ -576,42 +355,4 @@ func parseExpression(ctx *context.Context, exp string) (*SearchQuery, error) {
 		sq.Constraint = andConst(base, c)
 	}
 	return sq, nil
-}
-
-func whIntConstraint(mins, maxs string) *IntConstraint {
-	ic := &IntConstraint{}
-	if mins != "" {
-		if mins == "0" {
-			ic.ZeroMin = true
-		} else {
-			n, _ := strconv.Atoi(mins)
-			ic.Min = int64(n)
-		}
-	}
-	if maxs != "" {
-		if maxs == "0" {
-			ic.ZeroMax = true
-		} else {
-			n, _ := strconv.Atoi(maxs)
-			ic.Max = int64(n)
-		}
-	}
-	return ic
-}
-
-func mimeFromFormat(v string) string {
-	if strings.Contains(v, "/") {
-		return v
-	}
-	switch v {
-	case "jpg", "jpeg":
-		return "image/jpeg"
-	case "gif":
-		return "image/gif"
-	case "png":
-		return "image/png"
-	case "pdf":
-		return "application/pdf" // RFC 3778
-	}
-	return "???"
 }
