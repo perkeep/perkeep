5d4f2dfefef2ee165f5ffdeddb0b7bb0cb8d9f1b
Brad Fitzpatrick <brad@danga.com>
2014-04-17 10:20:05 -0700

    serverinit: let genconfig's update_golden flag, even if files matched
    Change-Id: I93cdd1b9a1945a975a1c8d2097e914ae2a8c77c1

--- pkg/serverinit/serverinit_test.go
+++ pkg/serverinit/serverinit_test.go
@@ -166,17 +166,17 @@ func testConfig(name string, t *testing.T) {
 	var got, want bytes.Buffer
 	prettyPrint(t, &got, lowLevelConf.Obj, 0)
 	prettyPrint(t, &want, wantConf, 0)
-	if got.String() != want.String() {
-		if *updateGolden {
-			contents, err := json.MarshalIndent(lowLevelConf.Obj, "", "\t")
-			if err != nil {
-				t.Fatal(err)
-			}
-			contents = canonicalizeGolden(t, contents)
-			if err := ioutil.WriteFile(wantFile, contents, 0644); err != nil {
-				t.Fatal(err)
-			}
+	if *updateGolden {
+		contents, err := json.MarshalIndent(lowLevelConf.Obj, "", "\t")
+		if err != nil {
+			t.Fatal(err)
 		}
+		contents = canonicalizeGolden(t, contents)
+		if err := ioutil.WriteFile(wantFile, contents, 0644); err != nil {
+			t.Fatal(err)
+		}
+	}
+	if got.String() != want.String() {
 		t.Errorf("test %s configurations differ.\nGot:\n%s\nWant:\n%s\nDiff (want -> got), %s:\n%s",
 			name, &got, &want, name, test.Diff(want.Bytes(), got.Bytes()))
 	}
