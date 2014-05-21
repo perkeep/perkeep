toshnix/camlistore

cmd/camget, cmd/camput, pkg/schema: Support FIFOs.
This patch:

1. Introduces a JSON schema for FIFOs (named pipes).
2. Adds support for uploading and restoring FIFOs to camput and camget
respectively.

It includes tests for the basic schema support in pkg/schema and
integration tests for camput and camget.

This CL does not add FIFO support to the HTTP server and fs (cammount
support) code. Those will be addressed in a subsequent patch.

Change-Id: Iab926073c36e653d25e693a559178bea9bf19b9a

pkg/schema/schema_test.go
@@ -21,7 +21,9 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
+	"syscall"
 	"testing"
 	"time"
 
@@ -482,5 +484,43 @@ func TestStaticFileAndStaticSymlink(t *testing.T) {
 
 	if want, got := target, sl.SymlinkTargetString(); got != want {
 		t.Fatalf("StaticSymlink.SymlinkTargetString(): Expected %s, got %s", want, got)
+	}
+}
+
+func TestStaticFIFO(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.SkipNow()
+	}
+
+	tdir, err := ioutil.TempDir("", "schema-test-")
+	if err != nil {
+		t.Fatalf("ioutil.TempDir(): %v", err)
+	}
+	defer os.RemoveAll(tdir)
+
+	fifoPath := filepath.Join(tdir, "fifo")
+	err = syscall.Mkfifo(fifoPath, 0660)
+	if err != nil {
+		t.Fatalf("syscall.Mkfifo(): %v", err)
+	}
+
+	fi, err := os.Lstat(fifoPath)
+	if err != nil {
+		t.Fatalf("os.Lstat(): %v", err)
+	}
+
+	bb := NewCommonFileMap(fifoPath, fi)
+	bb.SetType("FIFO")
+	bb.SetFileName(fifoPath)
+	blob := bb.Blob()
+	t.Logf("Got JSON for FIFO: %s\n", blob.JSON())
+
+	sf, ok := blob.AsStaticFile()
+	if !ok {
+		t.Fatalf("Blob.AsStaticFile(): Expected true, got false")
+	}
+	_, ok = sf.AsStaticFIFO()
+	if !ok {
+		t.Fatalf("StaticFile.AsStaticFIFO(): Expected true, got false")
 	}
 }

pkg/test/integration/camput_test.go
@@ -1,0 +1,67 @@
+/*
+Copyright 2013 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package integration
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"syscall"
+	"testing"
+
+	"camlistore.org/pkg/test"
+)
+
+// mkTmpFIFO makes a FIFO in a temporary directory and returns the
+// path it and a function to clean-up when done.
+func mkTmpFIFO(t *testing.T) (path string, cleanup func()) {
+	tdir, err := ioutil.TempDir("", "fifo-test-")
+	if err != nil {
+		t.Fatalf("iouti.TempDir(): %v", err)
+	}
+	cleanup = func() {
+		os.RemoveAll(tdir)
+	}
+
+	path = filepath.Join(tdir, "fifo")
+	err = syscall.Mkfifo(path, 0660)
+	if err != nil {
+		t.Fatalf("syscall.mkfifo(): %v", err)
+	}
+
+	return
+}
+
+// Test that `camput' can upload FIFOs correctly.
+func TestCamputFIFO(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.SkipNow()
+	}
+
+	fifo, cleanup := mkTmpFIFO(t)
+	defer cleanup()
+
+	// Can we successfully upload a FIFO?
+	w := test.GetWorld(t)
+	out := test.MustRunCmd(t, w.Cmd("camput", "file", fifo))
+
+	br := strings.Split(out, "\n")[0]
+	out = test.MustRunCmd(t, w.Cmd("camget", br))
+	t.Logf("Retrieved stored FIFO schema: %s", out)
+}

perkeep/perkeep

pkg/schema/schema_test.go
@@ -22,11 +22,13 @@ import (
 	"os"
 	"path/filepath"
 	"reflect"
+	"runtime"
 	"strings"
 	"testing"
 	"time"
 
 	"camlistore.org/pkg/blob"
+	"camlistore.org/pkg/osutil"
 	. "camlistore.org/pkg/test/asserts"
 )
 
@@ -524,5 +526,43 @@ func TestStaticFileAndStaticSymlink(t *testing.T) {
 
 	if want, got := target, sl.SymlinkTargetString(); got != want {
 		t.Fatalf("StaticSymlink.SymlinkTargetString(): Expected %s, got %s", want, got)
+	}
+}
+
+func TestStaticFIFO(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.SkipNow()
+	}
+
+	tdir, err := ioutil.TempDir("", "schema-test-")
+	if err != nil {
+		t.Fatalf("ioutil.TempDir(): %v", err)
+	}
+	defer os.RemoveAll(tdir)
+
+	fifoPath := filepath.Join(tdir, "fifo")
+	err = osutil.Mkfifo(fifoPath, 0660)
+	if err != nil {
+		t.Fatalf("osutil.Mkfifo(): %v", err)
+	}
+
+	fi, err := os.Lstat(fifoPath)
+	if err != nil {
+		t.Fatalf("os.Lstat(): %v", err)
+	}
+
+	bb := NewCommonFileMap(fifoPath, fi)
+	bb.SetType("fifo")
+	bb.SetFileName(fifoPath)
+	blob := bb.Blob()
+	t.Logf("Got JSON for fifo: %s\n", blob.JSON())
+
+	sf, ok := blob.AsStaticFile()
+	if !ok {
+		t.Fatalf("Blob.AsStaticFile(): Expected true, got false")
+	}
+	_, ok = sf.AsStaticFIFO()
+	if !ok {
+		t.Fatalf("StaticFile.AsStaticFIFO(): Expected true, got false")
 	}
 }

pkg/test/integration/camput_test.go
@@ -1,0 +1,67 @@
+/*
+Copyright 2013 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package integration
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"testing"
+
+	"camlistore.org/pkg/osutil"
+	"camlistore.org/pkg/test"
+)
+
+// mkTmpFIFO makes a fifo in a temporary directory and returns the
+// path it and a function to clean-up when done.
+func mkTmpFIFO(t *testing.T) (path string, cleanup func()) {
+	tdir, err := ioutil.TempDir("", "fifo-test-")
+	if err != nil {
+		t.Fatalf("iouti.TempDir(): %v", err)
+	}
+	cleanup = func() {
+		os.RemoveAll(tdir)
+	}
+
+	path = filepath.Join(tdir, "fifo")
+	err = osutil.Mkfifo(path, 0660)
+	if err != nil {
+		t.Fatalf("osutil.mkfifo(): %v", err)
+	}
+
+	return
+}
+
+// Test that `camput' can upload fifos correctly.
+func TestCamputFIFO(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.SkipNow()
+	}
+
+	fifo, cleanup := mkTmpFIFO(t)
+	defer cleanup()
+
+	// Can we successfully upload a fifo?
+	w := test.GetWorld(t)
+	out := test.MustRunCmd(t, w.Cmd("camput", "file", fifo))
+
+	br := strings.Split(out, "\n")[0]
+	out = test.MustRunCmd(t, w.Cmd("camget", br))
+	t.Logf("Retrieved stored fifo schema: %s", out)
+}