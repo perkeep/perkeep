d5fb600ac21ac022d2d61a99a567c4abbab5eaa9
Brad Fitzpatrick <brad@danga.com>
2014-06-02 15:55:10 +0900

    Better demo setup
    Change-Id: I61e55e3773e30332e6a7d3b426090fbe946a2462

--- dev/demo.sh
+++ dev/demo.sh
@@ -4,10 +4,15 @@
 # still permit using the dev-* scripts (which are normally slow and
 # noisy)
 
-go run make.go
-go install camlistore.org/dev/devcam
-export CAMLI_QUIET=1
-export CAMLI_FAST_DEV=1
+#go run make.go
+#go install camlistore.org/dev/devcam
+#export CAMLI_QUIET=1
+#export CAMLI_FAST_DEV=1
 
-#Or just:
+# Or just:
+# (This way is buggy in that the server selection doesn't let you also
+# pick an identity)
 # export CAMLI_DEFAULT_SERVER=dev
+
+# Better:
+export CAMLI_CONFIG_DIR=$HOME/src/camlistore.org/config/dev-client-dir-demo
