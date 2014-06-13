694d1b9dd474ec4e2a65e50dd9c049a231ce76e4
Brad Fitzpatrick <brad@danga.com>
2014-06-13 15:52:20 -0700

    importer: add Host.BlobSource and Object.SetAttrs2 to detect any changes
    Change-Id: I294df82a8446357d2ab260137e80b2aaec630827

--- pkg/importer/importer.go
+++ pkg/importer/importer.go
@@ -243,6 +243,7 @@ type Host struct {
 	baseURL      string
 	importerBase string
 	target       blobserver.StatReceiver
+	blobSource   blob.Fetcher // e.g. twitter reading zip file
 	search       *search.Handler
 	signer       *schema.Signer
 	uiPrefix     string // or empty if no UI handler
@@ -272,6 +273,7 @@ func (h *Host) InitHandler(hl blobserver.FindHandlerByTyper) error {
 		return errors.New("importer requires a 'root' handler with 'blobRoot' defined.")
 	}
 	h.target = rh.Storage
+	h.blobSource = rh.Storage
 
 	_, handler, _ = hl.FindHandlerByType("jsonsign")
 	if sigh, ok := handler.(*signhandler.Handler); ok {
@@ -504,6 +506,10 @@ func (h *Host) ImporterBaseURL() string {
 
 func (h *Host) Target() blobserver.StatReceiver {
 	return h.target
+}
+
+func (h *Host) BlobSource() blob.Fetcher {
+	return h.blobSource
 }
 
 func (h *Host) Search() *search.Handler {
@@ -1047,8 +1053,17 @@ func (o *Object) SetAttr(key, value string) error {
 	return nil
 }
 
-// SetAttrs sets multiple attributes. The provided keyval should be an even number of alternating key/value pairs to set.
+// SetAttrs sets multiple attributes. The provided keyval should be an
+// even number of alternating key/value pairs to set.
 func (o *Object) SetAttrs(keyval ...string) error {
+	_, err := o.SetAttrs2(keyval...)
+	return err
+}
+
+// SetAttrs2 sets multiple attributes and returns whether there were
+// any changes. The provided keyval should be an even number of
+// alternating key/value pairs to set.
+func (o *Object) SetAttrs2(keyval ...string) (changes bool, err error) {
 	if len(keyval)%2 == 1 {
 		panic("importer.SetAttrs: odd argument count")
 	}
@@ -1057,12 +1072,13 @@ func (o *Object) SetAttrs(keyval ...string) error {
 	for i := 0; i < len(keyval); i += 2 {
 		key, val := keyval[i], keyval[i+1]
 		if val != o.Attr(key) {
+			changes = true
 			g.Go(func() error {
 				return o.SetAttr(key, val)
 			})
 		}
 	}
-	return g.Err()
+	return changes, g.Err()
 }
 
 // SetAttrValues sets multi-valued attribute.
