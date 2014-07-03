c0ba41fbce7aef20001deb4674356b77d16cf4b2
Piotr S. Staszewski <p.staszewski@gmail.com>
2014-07-03 15:23:16 +0200

    magic: add file types and tests
    Additional mappings (taken from the file utility source code) and
    test cases. Included file license.
    
    Change-Id: I1dadff94c7c7d5280e12d82da61b7159f29eabe3

--- pkg/magic/magic.go
+++ pkg/magic/magic.go
@@ -3,7 +3,7 @@ Copyright 2011 Google Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
-nYou may obtain a copy of the License at
+You may obtain a copy of the License at
 
      http://www.apache.org/licenses/LICENSE-2.0
 
@@ -25,9 +25,12 @@ import (
 	"net/http"
 	"path/filepath"
 	"strings"
+
+	"go4.org/legal"
 )
 
 type prefixEntry struct {
+	offset int
 	prefix []byte
 	mtype  string
 }
@@ -35,42 +38,112 @@ type prefixEntry struct {
 // usable source: http://www.garykessler.net/library/file_sigs.html
 // mime types: http://www.iana.org/assignments/media-types/media-types.xhtml
 var prefixTable = []prefixEntry{
-	{[]byte("GIF87a"), "image/gif"},
-	{[]byte("GIF89a"), "image/gif"}, // TODO: Others?
-	{[]byte("\xff\xd8\xff\xe2"), "image/jpeg"},
-	{[]byte("\xff\xd8\xff\xe1"), "image/jpeg"},
-	{[]byte("\xff\xd8\xff\xe0"), "image/jpeg"},
-	{[]byte("\xff\xd8\xff\xdb"), "image/jpeg"},
-	{[]byte("\x49\x49\x2a\x00\x10\x00\x00\x00\x43\x52\x02"), "image/cr2"},
-	{[]byte{137, 'P', 'N', 'G', '\r', '\n', 26, 10}, "image/png"},
-	{[]byte{0x49, 0x20, 0x49}, "image/tiff"},
-	{[]byte{0x49, 0x49, 0x2A, 0}, "image/tiff"},
-	{[]byte{0x4D, 0x4D, 0, 0x2A}, "image/tiff"},
-	{[]byte{0x4D, 0x4D, 0, 0x2B}, "image/tiff"},
-	{[]byte("8BPS"), "image/vnd.adobe.photoshop"},
-	{[]byte("gimp xcf "), "image/xcf"},
-	{[]byte("-----BEGIN PGP PUBLIC KEY BLOCK---"), "text/x-openpgp-public-key"},
-	{[]byte("fLaC\x00\x00\x00"), "audio/flac"},
-	{[]byte{'I', 'D', '3'}, "audio/mpeg"},
-	{[]byte{0, 0, 1, 0xB7}, "video/mpeg"},
-	{[]byte{0, 0, 0, 0x14, 0x66, 0x74, 0x79, 0x70, 0x71, 0x74, 0x20, 0x20}, "video/quicktime"},
-	{[]byte{0, 0x6E, 0x1E, 0xF0}, "application/vnd.ms-powerpoint"},
-	{[]byte{0x1A, 0x45, 0xDF, 0xA3}, "video/webm"},
-	{[]byte("FLV\x01"), "application/vnd.adobe.flash.video"},
-	{[]byte{0x1F, 0x8B, 0x08}, "application/gzip"},
-	{[]byte{0x37, 0x7A, 0xBC, 0xAF, 0x27, 0x1C}, "application/x-7z-compressed"},
-	{[]byte("BZh"), "application/bzip2"},
-	{[]byte{0xFD, 0x37, 0x7A, 0x58, 0x5A, 0}, "application/x-xz"},
-	{[]byte{'P', 'K', 3, 4, 0x0A, 0, 2, 0}, "application/epub+zip"},
-	{[]byte{0xD0, 0xCF, 0x11, 0xE0, 0xA1, 0xB1, 0x1A, 0xE1}, "application/vnd.ms-word"},
-	{[]byte{'P', 'K', 3, 4, 0x0A, 0x14, 0, 6, 0}, "application/vnd.openxmlformats-officedocument.custom-properties+xml"},
-	{[]byte{'P', 'K', 3, 4}, "application/zip"},
-	{[]byte("%PDF"), "application/pdf"},
-	{[]byte("{rtf"), "text/rtf1"},
-	{[]byte("BEGIN:VCARD\x0D\x0A"), "text/vcard"},
-	{[]byte("Return-Path: "), "message/rfc822"},
+	{0, []byte("GIF87a"), "image/gif"},
+	{0, []byte("GIF89a"), "image/gif"}, // TODO: Others?
+	{0, []byte("\xff\xd8\xff\xe2"), "image/jpeg"},
+	{0, []byte("\xff\xd8\xff\xe1"), "image/jpeg"},
+	{0, []byte("\xff\xd8\xff\xe0"), "image/jpeg"},
+	{0, []byte("\xff\xd8\xff\xdb"), "image/jpeg"},
+	{0, []byte("\x49\x49\x2a\x00\x10\x00\x00\x00\x43\x52\x02"), "image/cr2"},
+	{0, []byte{137, 'P', 'N', 'G', '\r', '\n', 26, 10}, "image/png"},
+	{0, []byte{0x49, 0x20, 0x49}, "image/tiff"},
+	{0, []byte{0x49, 0x49, 0x2A, 0}, "image/tiff"},
+	{0, []byte{0x4D, 0x4D, 0, 0x2A}, "image/tiff"},
+	{0, []byte{0x4D, 0x4D, 0, 0x2B}, "image/tiff"},
+	{0, []byte("8BPS"), "image/vnd.adobe.photoshop"},
+	{0, []byte("gimp xcf "), "image/x-xcf"},
+	{0, []byte("-----BEGIN PGP PUBLIC KEY BLOCK---"), "text/x-openpgp-public-key"},
+	{0, []byte("fLaC\x00\x00\x00"), "audio/x-flac"},
+	{0, []byte{'I', 'D', '3'}, "audio/mpeg"},
+	{0, []byte{0, 0, 1, 0xB7}, "video/mpeg"},
+	{0, []byte{0, 0, 0, 0x14, 0x66, 0x74, 0x79, 0x70, 0x71, 0x74, 0x20, 0x20}, "video/quicktime"},
+	{0, []byte{0, 0x6E, 0x1E, 0xF0}, "application/vnd.ms-powerpoint"},
+	{0, []byte{0x1A, 0x45, 0xDF, 0xA3}, "video/webm"},
+	{0, []byte("FLV\x01"), "application/vnd.adobe.flash.video"},
+	{0, []byte{0x1F, 0x8B, 0x08}, "application/x-gzip"},
+	{0, []byte{0x37, 0x7A, 0xBC, 0xAF, 0x27, 0x1C}, "application/x-7z-compressed"},
+	{0, []byte("BZh"), "application/x-bzip2"},
+	{0, []byte{0xFD, 0x37, 0x7A, 0x58, 0x5A, 0}, "application/x-xz"},
+	{0, []byte{'P', 'K', 3, 4, 0x0A, 0, 2, 0}, "application/epub+zip"},
+	{0, []byte{0xD0, 0xCF, 0x11, 0xE0, 0xA1, 0xB1, 0x1A, 0xE1}, "application/vnd.ms-word"},
+	{0, []byte{'P', 'K', 3, 4, 0x0A, 0x14, 0, 6, 0}, "application/vnd.openxmlformats-officedocument.custom-properties+xml"},
+	{0, []byte{'P', 'K', 3, 4}, "application/zip"},
+	{0, []byte("%PDF"), "application/pdf"},
+	{0, []byte("{rtf"), "text/rtf1"},
+	{0, []byte("BEGIN:VCARD\x0D\x0A"), "text/vcard"},
+	{0, []byte("Return-Path: "), "message/rfc822"},
+
+	// Definition data extracted automatically from the file utility source code.
+	// See: http://darwinsys.com/file/ (version used: 5.19)
+	{4, []byte("moov"), "video/quicktime"},                           // Apple QuickTime
+	{4, []byte("mdat"), "video/quicktime"},                           // Apple QuickTime movie (unoptimized)
+	{8, []byte("isom"), "video/mp4"},                                 // MPEG v4 system, version 1
+	{8, []byte("mp41"), "video/mp4"},                                 // MPEG v4 system, version 1
+	{8, []byte("mp42"), "video/mp4"},                                 // MPEG v4 system, version 2
+	{8, []byte("mmp4"), "video/mp4"},                                 // MPEG v4 system, 3GPP Mobile
+	{8, []byte("3ge"), "video/3gpp"},                                 // MPEG v4 system, 3GPP
+	{8, []byte("3gg"), "video/3gpp"},                                 // MPEG v4 system, 3GPP
+	{8, []byte("3gp"), "video/3gpp"},                                 // MPEG v4 system, 3GPP
+	{8, []byte("3gs"), "video/3gpp"},                                 // MPEG v4 system, 3GPP
+	{8, []byte("3g2"), "video/3gpp2"},                                // MPEG v4 system, 3GPP2
+	{8, []byte("avc1"), "video/3gpp"},                                // MPEG v4 system, 3GPP JVT AVC
+	{0, []byte("MThd"), "audio/midi"},                                // Standard MIDI data
+	{0, []byte(".RMF\000\000\000"), "application/vnd.rn-realmedia"},  // RealMedia file
+	{0, []byte("MAC\040"), "audio/ape"},                              // Monkey's Audio compressed format
+	{0, []byte("MP+"), "audio/musepack"},                             // Musepack audio
+	{0, []byte("II\x1a\000\000\000HEAPCCDR"), "image/x-canon-crw"},   // Canon CIFF raw image data
+	{0, []byte("II\x2a\000\x10\000\000\000CR"), "image/x-canon-cr2"}, // Canon CR2 raw image data
+	{0, []byte("MMOR"), "image/x-olympus-orf"},                       // Olympus ORF raw image data, big-endian
+	{0, []byte("IIRO"), "image/x-olympus-orf"},                       // Olympus ORF raw image data, little-endian
+	{0, []byte("IIRS"), "image/x-olympus-orf"},                       // Olympus ORF raw image data, little-endian
+	{12, []byte("DJVM"), "image/vnd.djvu"},                           // DjVu multiple page document
+	{12, []byte("DJVU"), "image/vnd.djvu"},                           // DjVu image or single page document
+	{12, []byte("DJVI"), "image/vnd.djvu"},                           // DjVu shared document
+	{12, []byte("THUM"), "image/vnd.djvu"},                           // DjVu page thumbnails
+	{8, []byte("WAVE"), "audio/x-wav"},                               // WAVE audio
+	{8, []byte("AVI\040"), "video/x-msvideo"},                        // AVI
+	{0, []byte("OggS"), "application/ogg"},                           // Ogg data
+	{8, []byte("AIFF"), "audio/x-aiff"},                              // AIFF audio
+	{8, []byte("AIFC"), "audio/x-aiff"},                              // AIFF-C compressed audio
+	{8, []byte("8SVX"), "audio/x-aiff"},                              // 8SVX 8-bit sampled sound voice
+	{0, []byte("\000\001\000\000\000"), "application/x-font-ttf"},    // TrueType font data
+	{0, []byte("d8:announce"), "application/x-bittorrent"},           // BitTorrent file
 
 	// TODO(bradfitz): popular audio & video formats at least
+}
+
+func init() {
+	legal.RegisterLicense(`
+$File: LEGAL.NOTICE,v 1.15 2006/05/03 18:48:33 christos Exp $
+Copyright (c) Ian F. Darwin 1986, 1987, 1989, 1990, 1991, 1992, 1994, 1995.
+Software written by Ian F. Darwin and others;
+maintained 1994- Christos Zoulas.
+
+This software is not subject to any export provision of the United States
+Department of Commerce, and may be exported to any country or planet.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice immediately at the beginning of the file, without modification,
+   this list of conditions, and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ''AS IS'' AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.
+`)
 }
 
 // MIMEType returns the MIME type from the data in the provided header
@@ -79,8 +152,8 @@ var prefixTable = []prefixEntry{
 func MIMEType(hdr []byte) string {
 	hlen := len(hdr)
 	for _, pte := range prefixTable {
-		plen := len(pte.prefix)
-		if hlen > plen && bytes.Equal(hdr[:plen], pte.prefix) {
+		plen := pte.offset + len(pte.prefix)
+		if hlen > plen && bytes.Equal(hdr[pte.offset:plen], pte.prefix) {
 			return pte.mtype
 		}
 	}
--- pkg/magic/magic_test.go
+++ pkg/magic/magic_test.go
@@ -17,9 +17,11 @@ limitations under the License.
 package magic
 
 import (
+	"bytes"
 	"errors"
 	"io"
 	"io/ioutil"
+	"os/exec"
 	"strings"
 	"testing"
 )
@@ -34,18 +36,29 @@ var tests = []magicTest{
 	{fileName: "smile.png", want: "image/png"},
 	{fileName: "smile.psd", want: "image/vnd.adobe.photoshop"},
 	{fileName: "smile.tiff", want: "image/tiff"},
-	{fileName: "smile.xcf", want: "image/xcf"},
+	{fileName: "smile.xcf", want: "image/x-xcf"},
 	{fileName: "smile.gif", want: "image/gif"},
-	{fileName: "foo.tar.gz", want: "application/gzip"},
+	{fileName: "foo.tar.gz", want: "application/x-gzip"},
 	{fileName: "foo.tar.xz", want: "application/x-xz"},
-	{fileName: "foo.tbz2", want: "application/bzip2"},
+	{fileName: "foo.tbz2", want: "application/x-bzip2"},
 	{fileName: "foo.zip", want: "application/zip"},
 	{fileName: "magic.pdf", want: "application/pdf"},
+	{fileName: "hello.mp4", want: "video/mp4"},
+	{fileName: "hello.3gp", want: "video/3gpp"},
+	{fileName: "hello.avi", want: "video/x-msvideo"},
+	{fileName: "hello.mov", want: "video/quicktime"},
+	{fileName: "silence.wav", want: "audio/x-wav"},
+	{fileName: "silence.flac", want: "audio/x-flac"},
 	{data: "<html>foo</html>", want: "text/html"},
 	{data: "\xff", want: ""},
 }
 
 func TestMagic(t *testing.T) {
+	var hasFile bool
+	if _, err := exec.LookPath("file"); err == nil {
+		hasFile = true
+	}
+
 	for i, tt := range tests {
 		var err error
 		data := []byte(tt.data)
@@ -60,7 +73,37 @@ func TestMagic(t *testing.T) {
 		if mime != tt.want {
 			t.Errorf("%d. got %q; want %q", i, mime, tt.want)
 		}
+		if !hasFile {
+			continue
+		}
+		fmime, ok := runFileCmd(data)
+		if ok && fmime != tt.want {
+			t.Logf("%d. warning: got %q via file; want %q", i, fmime, tt.want)
+		}
 	}
+}
+
+// runFileCmd runs the file utility and returns the mime-type and true
+// or an empty string and false on any error. It also mimics the MIMEType
+// behaviour.
+func runFileCmd(data []byte) (string, bool) {
+	cmd := exec.Command("file", "--mime-type", "-")
+	cmd.Stdin = bytes.NewReader(data)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	if err := cmd.Run(); err != nil {
+		return "", false
+	}
+	outString := out.String()
+	idx := strings.LastIndex(outString, " ")
+	if idx == -1 {
+		return "", false
+	}
+	mime := outString[idx+1 : len(outString)-1]
+	if mime != "application/octet-stream" && mime != "text/plain" {
+		return mime, true
+	}
+	return "", true
 }
 
 func TestMIMETypeFromReader(t *testing.T) {
