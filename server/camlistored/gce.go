b6cff261390a9850f758dd7376dcf7c3c496b93a
Brad Fitzpatrick <brad@danga.com>
2014-08-06 17:00:58 -0700

    camlistored: register a jsonconfig _gce_instance_meta lookup func
    Change-Id: I1f3201be49aed32ac572282ffa4581ce4c71184f

---
+++ server/camlistored/gce.go
@@ -0,0 +1,45 @@
+/*
+Copyright 2014 The Camlistore Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"errors"
+	"fmt"
+
+	"camlistore.org/pkg/jsonconfig"
+	"camlistore.org/third_party/github.com/bradfitz/gce"
+)
+
+func init() {
+	if !gce.OnGCE() {
+		return
+	}
+	jsonconfig.RegisterFunc("_gce_instance_meta", func(c *jsonconfig.ConfigParser, v []interface{}) (interface{}, error) {
+		if len(v) != 1 {
+			return nil, errors.New("only 1 argument supported after _gce_instance_meta")
+		}
+		attr, ok := v[0].(string)
+		if !ok {
+			return nil, errors.New("expected argument after _gce_instance_meta to be a string")
+		}
+		val, err := gce.InstanceAttributeValue(attr)
+		if err != nil {
+			return nil, fmt.Errorf("error reading GCE instance attribute %q: %v", attr, err)
+		}
+		return val, nil
+	})
+}
