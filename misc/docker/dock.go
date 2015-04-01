5ee33643fd1f0754d730580c6d04a72a7d75aa60
Brad Fitzpatrick <brad@danga.com>
2015-04-01 03:48:12 -0700

    More dock.go image building work, with mpl
    Change-Id: I290d9364a49c95c5c5f35864f94d04780ae30843

--- misc/docker/dock.go
+++ misc/docker/dock.go
@@ -18,15 +18,27 @@ limitations under the License.
 package main
 
 import (
+	"flag"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 
 	"camlistore.org/pkg/osutil"
 )
 
+var (
+	rev = flag.String("rev", "4e8413c5012c", "Camlistore revision to build (tag or commit hash")
+)
+
 func main() {
+	flag.Parse()
+	if flag.NArg() != 0 {
+		log.Fatalf("Bogus usage. dock does not currently take any arguments.")
+	}
+
 	camDir, err := osutil.GoPackagePath("camlistore.org")
 	if err != nil {
 		log.Fatalf("Error looking up camlistore.org dir: %v", err)
@@ -39,5 +51,74 @@ func main() {
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
 		log.Fatalf("Error building camlistore/go: %v", err)
+	}
+
+	// TODO(mpl): build djpeg-static here, like camlistore/go
+	// above (and prefix the -t value with "camlistore/")
+	// (And pull above into a func)
+
+	repl := strings.NewReplacer(
+		"[[REV]]", *rev,
+	)
+
+	// ctxDir is where we run "docker build" to produce the final
+	// "FROM scratch" Docker image.
+	ctxDir, err := ioutil.TempDir("", "camli-build")
+	if err != nil {
+		log.Fatal(err)
+	}
+	check(os.Mkdir(filepath.Join(ctxDir, "/camlistore.org"), 0755))
+	defer os.RemoveAll(ctxDir)
+
+	cmd = exec.Command("docker", "run",
+		"--rm",
+		"--volume="+ctxDir+"/camlistore.org:/OUT",
+		"camlistore/go", "/bin/bash", "-c", repl.Replace(`
+
+# TODO(bradfitz,mpl): rewrite this shell into a Go program that's
+# baked into the camlistore/go image, and then all this shell becomes:
+# /usr/local/bin/build-camlistore-server $REV
+# (and it would still write to /OUT)
+
+     set -e
+     set -x
+     export GOPATH=/gopath;
+     export PATH=/usr/local/go/bin:$PATH;
+     mkdir -p /OUT/bin && 
+     mkdir -p /OUT/server/camlistored && 
+     mkdir -p /gopath/src/camlistore.org && 
+     cd /gopath/src/camlistore.org &&
+     curl --silent https://camlistore.googlesource.com/camlistore/+archive/[[REV]].tar.gz |
+           tar -zxv &&
+     CGO_ENABLED=0 go build -o /OUT/bin/camlistored -x --ldflags="-w -d -linkmode internal" --tags=netgo camlistore.org/server/camlistored &&
+     mv /gopath/src/camlistore.org/server/camlistored/ui /OUT/server/camlistored/ui &&
+     find /gopath/src/camlistore.org/third_party -type f -name '*.go' -exec rm {} \; &&
+     mv /gopath/src/camlistore.org/third_party /OUT/third_party &&
+     echo DONE
+`))
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("Error building camlistored in go container: %v", err)
+	}
+
+	// Copy Dockerfile into the temp dir.
+	serverDockerFile, err := ioutil.ReadFile(filepath.Join(dockDir, "server", "Dockerfile"))
+	check(err)
+	check(ioutil.WriteFile(filepath.Join(ctxDir, "Dockerfile"), serverDockerFile, 0644))
+
+	// TODO(mpl): copy the djpeg out
+	cmd = exec.Command("docker", "build", "-t", "camlistore/server", ".")
+	cmd.Dir = ctxDir
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("Error building camlistore/server: %v", err)
+	}
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
 	}
 }
