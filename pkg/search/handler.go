perkeep/perkeep

commit df7cc0b45332a0ca7f762cbba3ab4133aaea2d5e

pkg/search: replace JSON maps with structs in search handler responses
This change is related to Issue #539.

Change-Id: I588cbeac8867eb565e43813b5613e476f5f9d89d

pkg/search/handler.go
@@ -202,10 +202,11 @@ func (sh *Handler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	}
 
 	// TODO: discovery for the endpoints & better error message with link to discovery info
-	ret := jsonMap()
-	ret["error"] = "Unsupported search path or method"
-	ret["errorType"] = "input"
-	httputil.ReturnJSON(rw, ret)
+	ret := camtypes.SearchErrorResponse{
+		Error:     "Unsupported search path or method",
+		ErrorType: "input",
+	}
+	httputil.ReturnJSON(rw, &ret)
 }
 
 // sanitizeNumResults takes n as a requested number of search results and sanitizes it.
@@ -643,28 +644,24 @@ func (sh *Handler) serveClaims(rw http.ResponseWriter, req *http.Request) {
 }
 
 func (sh *Handler) serveFiles(rw http.ResponseWriter, req *http.Request) {
-	ret := jsonMap()
-	defer httputil.ReturnJSON(rw, ret)
+	var ret camtypes.FileSearchResponse
+	defer httputil.ReturnJSON(rw, &ret)
 
 	br, ok := blob.Parse(req.FormValue("wholedigest"))
 	if !ok {
-		ret["error"] = "Missing or invalid 'wholedigest' param"
-		ret["errorType"] = "input"
+		ret.Error = "Missing or invalid 'wholedigest' param"
+		ret.ErrorType = "input"
 		return
 	}
 
 	files, err := sh.index.ExistingFileSchemas(br)
 	if err != nil {
-		ret["error"] = err.Error()
-		ret["errorType"] = "server"
+		ret.Error = err.Error()
+		ret.ErrorType = "server"
 		return
 	}
 
-	strList := []string{}
-	for _, br := range files {
-		strList = append(strList, br.String())
-	}
-	ret["files"] = strList
+	ret.Files = files
 	return
 }
 

pkg/types/camtypes/search.go
@@ -239,3 +239,16 @@ type BlobMeta struct {
 
 	// TODO(bradfitz): change CamliTypethis *string to save 8 bytes
 }
+
+// SearchErrorResponse is the JSON error response for a search request.
+type SearchErrorResponse struct {
+	Error     string `json:"error,omitempty"`     // The error message.
+	ErrorType string `json:"errorType,omitempty"` // The type of the error.
+}
+
+// FileSearchResponse is the JSON response to a file search request.
+type FileSearchResponse struct {
+	SearchErrorResponse
+
+	Files []blob.Ref `json:"files,omitempty"` // Refs of the result files.
+}

commit 01cddcc8d557d14fb3cba5de28871d4dbfefc26b

pkg/search: fix null "files" in file search response
The "files" field of a file search response was always non-null (empty JSON array in case of error or no result).
This behaviour was broken by df7cc0b45332a0ca7f762cbba3ab4133aaea2d5e, so this CL is restoring it.

Fixes #619

Change-Id: I8e4762d1f5a2dba4f7cf06d8ca164392c4bf6d6a

pkg/search/handler.go
@@ -661,6 +661,11 @@ func (sh *Handler) serveFiles(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	// the ui code expects an object
+	if files == nil {
+		files = []blob.Ref{}
+	}
+
 	ret.Files = files
 	return
 }

pkg/types/camtypes/search.go
@@ -250,5 +250,5 @@ type SearchErrorResponse struct {
 type FileSearchResponse struct {
 	SearchErrorResponse
 
-	Files []blob.Ref `json:"files,omitempty"` // Refs of the result files.
+	Files []blob.Ref `json:"files"` // Refs of the result files. Never nil.
 }