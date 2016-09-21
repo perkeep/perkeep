perkeep/perkeep

commit 383f118ccf6ae6e082dc76f5c3b3647614a33684

pkg/server: validate that share hops are only in expected places in schema blobs
Fixes camlistore/camlistore#228

pkg/server/share_test.go
@@ -30,87 +30,165 @@ import (
 	"camlistore.org/pkg/test"
 )
 
+type shareTester struct {
+	t          *testing.T
+	sto        *test.Fetcher
+	handler    *shareHandler
+	sleeps     int
+	rec        *httptest.ResponseRecorder
+	restoreLog func()
+}
+
+func newShareTester(t *testing.T) *shareTester {
+	sto := new(test.Fetcher)
+	st := &shareTester{
+		t:          t,
+		sto:        sto,
+		handler:    &shareHandler{fetcher: sto},
+		restoreLog: test.TLog(t),
+	}
+	timeSleep = func(d time.Duration) {
+		st.sleeps++
+	}
+	return st
+}
+
+func (st *shareTester) done() {
+	timeSleep = time.Sleep
+	st.restoreLog()
+}
+
+func (st *shareTester) slept() bool {
+	v := st.sleeps > 0
+	st.sleeps = 0
+	return v
+}
+
+func (st *shareTester) putRaw(ref blob.Ref, data string) {
+	if _, err := blobserver.Receive(st.sto, ref, strings.NewReader(data)); err != nil {
+		st.t.Fatal(err)
+	}
+}
+
+func (st *shareTester) put(blob *schema.Blob) {
+	st.putRaw(blob.BlobRef(), blob.JSON())
+}
+
+func (st *shareTester) get(path string) *shareError {
+	st.rec = httptest.NewRecorder()
+	req, err := http.NewRequest("GET", "http://unused/"+path, nil)
+	if err != nil {
+		st.t.Fatalf("NewRequest(path=%q): %v", path, err)
+	}
+	if err := st.handler.serveHTTP(st.rec, req); err != nil {
+		return err.(*shareError)
+	}
+	return nil
+}
+
+func (st *shareTester) testGet(path string, wantErr errorCode) {
+	gotErr := st.get(path)
+	if wantErr != noError {
+		if gotErr == nil || gotErr.code != wantErr {
+			st.t.Errorf("Fetching %s, error = %v; want %v", path, gotErr, wantErr)
+		}
+	} else {
+		if gotErr != nil {
+			st.t.Errorf("Fetching %s, error = %v; want success", path, gotErr)
+		}
+	}
+}
+
 func TestHandleGetViaSharing(t *testing.T) {
-	sto := &test.Fetcher{}
-	handler := &shareHandler{fetcher: sto}
-	var wr *httptest.ResponseRecorder
+	st := newShareTester(t)
+	defer st.done()
 
-	putRaw := func(ref blob.Ref, data string) {
-		if _, err := blobserver.Receive(sto, ref, strings.NewReader(data)); err != nil {
-			t.Fatal(err)
-		}
-	}
-
-	put := func(blob *schema.Blob) {
-		putRaw(blob.BlobRef(), blob.JSON())
-	}
-
-	get := func(path string) *shareError {
-		wr = httptest.NewRecorder()
-		req, _ := http.NewRequest("GET", "http://unused/"+path, nil)
-		err := handler.serveHTTP(wr, req)
-		if err != nil {
-			return err.(*shareError)
-		}
-		return nil
-	}
-
-	testGet := func(path string, expectedError errorCode) {
-		err := get(path)
-		if expectedError != noError {
-			if err == nil || err.code != expectedError {
-				t.Errorf("Fetching %s, expected error %#v, but got %#v", path, expectedError, err)
-			}
-		} else {
-			if err != nil {
-				t.Errorf("Fetching %s, expected success but got %#v", path, err)
-			}
-		}
-
-		if wr.HeaderMap.Get("Access-Control-Allow-Origin") != "*" {
-			t.Errorf("Fetching %s, share response did not contain expected CORS header", path)
-		}
-	}
-
-	content := "monkey"
+	content := "monkey" // the secret
 	contentRef := blob.SHA1FromString(content)
 
-	// For the purposes of following the via chain, the only thing that
-	// matters is that the content of each link contains the name of the
-	// next link.
-	link := contentRef.String()
+	link := fmt.Sprintf(`{"camliVersion": 1,
+"camliType": "file",
+"parts": [
+   {"blobRef": "%v", "size": %d}
+]}`, contentRef, len(content))
 	linkRef := blob.SHA1FromString(link)
 
 	share := schema.NewShareRef(schema.ShareHaveRef, false).
 		SetShareTarget(linkRef).
 		SetSigner(blob.SHA1FromString("irrelevant")).
 		SetRawStringField("camliSig", "alsounused")
+	shareRef := func() blob.Ref { return share.Blob().BlobRef() }
 
-	testGet(share.Blob().BlobRef().String(), shareFetchFailed)
+	t.Logf("Checking share blob doesn't yet exist...")
+	st.testGet(shareRef().String(), shareFetchFailed)
+	if !st.slept() {
+		t.Error("expected sleep after miss")
+	}
+	st.put(share.Blob())
+	t.Logf("Checking share blob now exists...")
+	st.testGet(shareRef().String(), noError)
 
-	put(share.Blob())
-	testGet(fmt.Sprintf("%s?via=%s", contentRef, share.Blob().BlobRef()), shareTargetInvalid)
+	t.Logf("Checking we can't get the content directly via the share...")
+	st.testGet(fmt.Sprintf("%s?via=%s", contentRef, shareRef()), shareTargetInvalid)
 
-	putRaw(linkRef, link)
-	testGet(linkRef.String(), shareReadFailed)
-	testGet(share.Blob().BlobRef().String(), noError)
-	testGet(fmt.Sprintf("%s?via=%s", linkRef, share.Blob().BlobRef()), noError)
-	testGet(fmt.Sprintf("%s?via=%s,%s", contentRef, share.Blob().BlobRef(), linkRef), shareNotTransitive)
+	t.Logf("Checking we can't get the link (file) blob directly...")
+	st.putRaw(linkRef, link)
+	st.testGet(linkRef.String(), shareBlobInvalid)
 
+	t.Logf("Checking we can get the link (file) blob fia the share...")
+	st.testGet(fmt.Sprintf("%s?via=%s", linkRef, shareRef()), noError)
+
+	t.Logf("Checking we can't get the link (file) blob fia the non-transitive share...")
+	st.testGet(fmt.Sprintf("%s?via=%s,%s", contentRef, shareRef(), linkRef), shareNotTransitive)
+
+	// TODO: new test?
 	share.SetShareIsTransitive(true)
-	put(share.Blob())
-	testGet(fmt.Sprintf("%s?via=%s,%s", linkRef, share.Blob().BlobRef(), linkRef), viaChainInvalidLink)
+	st.put(share.Blob())
+	st.testGet(fmt.Sprintf("%s?via=%s,%s", linkRef, shareRef(), linkRef), viaChainInvalidLink)
 
-	putRaw(contentRef, content)
-	testGet(fmt.Sprintf("%s?via=%s,%s", contentRef, share.Blob().BlobRef(), linkRef), noError)
+	st.putRaw(contentRef, content)
+	st.testGet(fmt.Sprintf("%s?via=%s,%s", contentRef, shareRef(), linkRef), noError)
 
+	// new test?
 	share.SetShareExpiration(time.Now().Add(-time.Duration(10) * time.Minute))
-	put(share.Blob())
-	testGet(fmt.Sprintf("%s?via=%s,%s", contentRef, share.Blob().BlobRef(), linkRef), shareExpired)
+	st.put(share.Blob())
+	st.testGet(fmt.Sprintf("%s?via=%s,%s", contentRef, shareRef(), linkRef), shareExpired)
 
 	share.SetShareExpiration(time.Now().Add(time.Duration(10) * time.Minute))
-	put(share.Blob())
-	testGet(fmt.Sprintf("%s?via=%s,%s", contentRef, share.Blob().BlobRef(), linkRef), noError)
-
-	// TODO(aa): assemble
+	st.put(share.Blob())
+	st.testGet(fmt.Sprintf("%s?via=%s,%s", contentRef, shareRef(), linkRef), noError)
 }
+
+// Issue 228: only follow transitive blobref links in known trusted schema fields.
+func TestSharingTransitiveSafety(t *testing.T) {
+	st := newShareTester(t)
+	defer st.done()
+
+	content := "the secret"
+	contentRef := blob.SHA1FromString(content)
+
+	// User-injected blob, somehow.
+	evilClaim := fmt.Sprintf("Some payload containing the ref: %v", contentRef)
+	evilClaimRef := blob.SHA1FromString(evilClaim)
+
+	share := schema.NewShareRef(schema.ShareHaveRef, false).
+		SetShareTarget(evilClaimRef).
+		SetShareIsTransitive(true).
+		SetSigner(blob.SHA1FromString("irrelevant")).
+		SetRawStringField("camliSig", "alsounused")
+	shareRef := func() blob.Ref { return share.Blob().BlobRef() }
+
+	st.put(share.Blob())
+	st.putRaw(contentRef, content)
+	st.putRaw(evilClaimRef, evilClaim)
+
+	st.testGet(shareRef().String(), noError)
+	st.testGet(fmt.Sprintf("%s?via=%s", evilClaimRef, shareRef()), noError)
+
+	st.testGet(fmt.Sprintf("%s?via=%s,%s", contentRef, shareRef(), evilClaimRef), viaChainInvalidLink)
+	if !st.slept() {
+		t.Error("expected sleep after miss")
+	}
+}
+
+// TODO(aa): test the "assemble" mode too.

pkg/server/share.go
@@ -40,8 +40,8 @@ import (
 type responseType int
 
 const (
-	badRequest responseType = iota
-	unauthorizedRequest
+	badRequest          responseType = 400
+	unauthorizedRequest responseType = 401
 )
 
 type errorCode int
@@ -64,6 +64,31 @@ const (
 	viaChainReadFailed
 )
 
+var errorCodeStr = [...]string{
+	noError:               "noError",
+	assembleNonTransitive: "assembleNonTransitive",
+	invalidMethod:         "invalidMethod",
+	invalidURL:            "invalidURL",
+	invalidVia:            "invalidVia",
+	shareBlobInvalid:      "shareBlobInvalid",
+	shareBlobTooLarge:     "shareBlobTooLarge",
+	shareExpired:          "shareExpired",
+	shareFetchFailed:      "shareFetchFailed",
+	shareReadFailed:       "shareReadFailed",
+	shareTargetInvalid:    "shareTargetInvalid",
+	shareNotTransitive:    "shareNotTransitive",
+	viaChainFetchFailed:   "viaChainFetchFailed",
+	viaChainInvalidLink:   "viaChainInvalidLink",
+	viaChainReadFailed:    "viaChainReadFailed",
+}
+
+func (ec errorCode) String() string {
+	if ec < 0 || int(ec) >= len(errorCodeStr) || errorCodeStr[ec] == "" {
+		return fmt.Sprintf("ErrCode#%d", int(ec))
+	}
+	return errorCodeStr[ec]
+}
+
 type shareError struct {
 	code     errorCode
 	response responseType
@@ -71,7 +96,7 @@ type shareError struct {
 }
 
 func (e *shareError) Error() string {
-	return e.message
+	return fmt.Sprintf("share: %v (code=%v, type=%v)", e.message, e.code, e.response)
 }
 
 func unauthorized(code errorCode, format string, args ...interface{}) *shareError {
@@ -85,42 +110,47 @@ const fetchFailureDelay = 200 * time.Millisecond
 // ShareHandler handles the requests for "share" (and shared) blobs.
 type shareHandler struct {
 	fetcher blob.Fetcher
+	log     bool
 }
 
 func init() {
 	blobserver.RegisterHandlerConstructor("share", newShareFromConfig)
 }
 
-func newShareFromConfig(ld blobserver.Loader, conf jsonconfig.Obj) (h http.Handler, err error) {
+func newShareFromConfig(ld blobserver.Loader, conf jsonconfig.Obj) (http.Handler, error) {
 	blobRoot := conf.RequiredString("blobRoot")
 	if blobRoot == "" {
 		return nil, errors.New("No blobRoot defined for share handler")
 	}
-	if err = conf.Validate(); err != nil {
-		return
+	if err := conf.Validate(); err != nil {
+		return nil, err
 	}
 
-	share := &shareHandler{}
 	bs, err := ld.GetStorage(blobRoot)
 	if err != nil {
-		return nil, fmt.Errorf("Share handler's blobRoot of %q error: %v", blobRoot, err)
+		return nil, fmt.Errorf("failed to get share handler's storage at %q: %v", blobRoot, err)
 	}
 	fetcher, ok := bs.(blob.Fetcher)
 	if !ok {
-		return nil, errors.New("Share handler's storage not a Fetcher.")
+		return nil, errors.New("share handler's storage not a Fetcher.")
 	}
-	share.fetcher = fetcher
-	return share, nil
+	sh := &shareHandler{
+		fetcher: fetcher,
+		log:     true,
+	}
+	return sh, nil
 }
 
+var timeSleep = time.Sleep // for tests
+
 // Unauthenticated user.  Be paranoid.
-func handleGetViaSharing(conn http.ResponseWriter, req *http.Request,
+func handleGetViaSharing(rw http.ResponseWriter, req *http.Request,
 	blobRef blob.Ref, fetcher blob.Fetcher) error {
 	if !httputil.IsGet(req) {
 		return &shareError{code: invalidMethod, response: badRequest, message: "Invalid method"}
 	}
 
-	conn.Header().Set("Access-Control-Allow-Origin", "*")
+	rw.Header().Set("Access-Control-Allow-Origin", "*")
 
 	viaPathOkay := false
 	startTime := time.Now()
@@ -129,7 +159,7 @@ func handleGetViaSharing(conn http.ResponseWriter, req *http.Request,
 			// Insert a delay, to hide timing attacks probing
 			// for the existence of blobs.
 			sleep := fetchFailureDelay - (time.Now().Sub(startTime))
-			time.Sleep(sleep)
+			timeSleep(sleep)
 		}
 	}()
 	viaBlobs := make([]blob.Ref, 0)
@@ -164,7 +194,7 @@ func handleGetViaSharing(conn http.ResponseWriter, req *http.Request,
 			}
 			share, ok := blob.AsShare()
 			if !ok {
-				return unauthorized(shareBlobInvalid, "Fetch chain 0 of %s wasn't a valid Share", br)
+				return unauthorized(shareBlobInvalid, "Fetch chain 0 of %s wasn't a valid Share (is %q)", br, blob.Type())
 			}
 			if share.IsExpired() {
 				return unauthorized(shareExpired, "Share is expired")
@@ -183,19 +213,19 @@ func handleGetViaSharing(conn http.ResponseWriter, req *http.Request,
 			// not the first thing in the chain)
 			continue
 		default:
-			file, _, err := fetcher.Fetch(br)
+			rc, _, err := fetcher.Fetch(br)
 			if err != nil {
 				return unauthorized(viaChainFetchFailed, "Fetch chain %d of %s failed: %v", i, br, err)
 			}
-			defer file.Close()
-			lr := io.LimitReader(file, schema.MaxSchemaBlobSize)
+			defer rc.Close()
+			lr := io.LimitReader(rc, schema.MaxSchemaBlobSize)
 			slurpBytes, err := ioutil.ReadAll(lr)
 			if err != nil {
 				return unauthorized(viaChainReadFailed,
 					"Fetch chain %d of %s failed in slurp: %v", i, br, err)
 			}
-			saught := fetchChain[i+1].String()
-			if bytes.Index(slurpBytes, []byte(saught)) == -1 {
+			saught := fetchChain[i+1]
+			if !bytesHaveSchemaLink(br, slurpBytes, saught) {
 				return unauthorized(viaChainInvalidLink,
 					"Fetch chain %d of %s failed; no reference to %s", i, br, saught)
 			}
@@ -210,9 +240,9 @@ func handleGetViaSharing(conn http.ResponseWriter, req *http.Request,
 			Fetcher: fetcher,
 			// TODO(aa): It would be nice to specify a local cache here, as the UI handler does.
 		}
-		dh.ServeHTTP(conn, req, blobRef)
+		dh.ServeHTTP(rw, req, blobRef)
 	} else {
-		gethandler.ServeBlobRef(conn, req, blobRef, fetcher)
+		gethandler.ServeBlobRef(rw, req, blobRef, fetcher)
 	}
 	viaPathOkay = true
 	return nil
@@ -238,7 +268,9 @@ func (h *shareHandler) serveHTTP(rw http.ResponseWriter, req *http.Request) erro
 		case badRequest:
 			httputil.BadRequestError(rw, err.Error())
 		case unauthorizedRequest:
-			log.Print(err)
+			if h.log {
+				log.Print(err)
+			}
 			auth.SendUnauthorized(rw, req)
 		}
 	}
@@ -247,4 +279,41 @@ func (h *shareHandler) serveHTTP(rw http.ResponseWriter, req *http.Request) erro
 
 func (h *shareHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	h.serveHTTP(rw, req)
+}
+
+// bytesHaveSchemaLink reports whether bb is a valid Camlistore schema
+// blob and has target somewhere in a schema field used to represent a
+// Merkle-tree-ish file or directory.
+func bytesHaveSchemaLink(br blob.Ref, bb []byte, target blob.Ref) bool {
+	// Fast path for no:
+	if !bytes.Contains(bb, []byte(target.String())) {
+		return false
+	}
+	b, err := schema.BlobFromReader(br, bytes.NewReader(bb))
+	if err != nil {
+		return false
+	}
+	typ := b.Type()
+	switch typ {
+	case "file", "blob":
+		for _, bp := range b.ByteParts() {
+			if bp.BlobRef.Valid() {
+				return bp.BlobRef == target
+			}
+			if bp.BytesRef.Valid() {
+				return bp.BytesRef == target
+			}
+		}
+	case "directory":
+		if d, ok := b.DirectoryEntries(); ok {
+			return d == target
+		}
+	case "static-set":
+		for _, m := range b.StaticSetMembers() {
+			if m == target {
+				return true
+			}
+		}
+	}
+	return false
 }

commit 10f96c99d4c08908e9996ff84ac6956d607be7b8

pkg/test/integration: share large file test
We would have needed a sharing test with a file large enough to have
several parts to detect the bugs introduced in rev
383f118ccf6ae6e082dc76f5c3b3647614a33684 and fixed in rev
ac4f053414b1e740de666c5ce828e3b0f6de3817

The sharing test introduced in this change is such a test.

Fixes #527

Change-Id: I7cd8a0b9b8e2acf794723f30ae38bd97d9eced80

pkg/test/integration/share_test.go
@@ -31,6 +31,12 @@ func TestFileSharing(t *testing.T) {
 	share(t, "share_test.go")
 }
 
+// like TestFileSharing, but with a file large enough to have several parts,
+// including some bytesRef parts.
+func TestFileWithBytesSharing(t *testing.T) {
+	share(t, test.GetWorld(t).ServerBinary())
+}
+
 func TestDirSharing(t *testing.T) {
 	share(t, filepath.FromSlash("../integration"))
 }

pkg/test/world.go
@@ -371,3 +371,8 @@ func (w *World) SecretRingFile() string {
 
 // SearchHandlerPath returns the path to the search handler, with trailing slash.
 func (w *World) SearchHandlerPath() string { return "/my-search/" }
+
+// ServerBinary returns the location of the camlistored binary running for this World.
+func (w *World) ServerBinary() string {
+	return filepath.Join(w.camRoot, "bin", "camlistored")
+}