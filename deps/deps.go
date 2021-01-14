678a9f910bc86cd2f67d55bcd9161f564f4b3b5d
aviau <alexandre@alexandreviau.net>
2021-01-14 12:33:05 -0500

    deps.go: add build flag
    We never really want to build this, but it makes `go get ./...`
    complain because it imports programs (and not modules), so add
    a build flag.
    
    FYI this is known as the "tools.go" pattern, as discussed here:
     - https://github.com/golang/go/issues/25922

--- deps/deps.go
+++ deps/deps.go
@@ -1,3 +1,5 @@
+// +build deps
+
 // Package deps depends on go modules in order to work around the fact that
 // `go mod` subcommands will end up removing dependencies from the vendor directory
 // if they are not referenced in any packages.
