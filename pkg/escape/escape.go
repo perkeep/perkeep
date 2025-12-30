5ba312598093d3ea2a311b72d1542ed956730624
Brad Fitzpatrick <brad@danga.com>
2025-12-30 07:47:48 -0800

    pkg/index: make Corpus.kvClaimBytes not allocate
        goos: darwin
        goarch: arm64
        pkg: perkeep.org/pkg/index
        cpu: Apple M4 Max
        BenchmarkKvClaim-16               330669              3617 ns/op             768 B/op         24 allocs/op
        BenchmarkKvClaim-16               332058              3561 ns/op             768 B/op         24 allocs/op
        BenchmarkKvClaim-16               334334              3551 ns/op             768 B/op         24 allocs/op
        BenchmarkKvClaim-16               335089              3555 ns/op             768 B/op         24 allocs/op
        BenchmarkKvClaim-16               331881              3501 ns/op             768 B/op         24 allocs/op
        BenchmarkKvClaimBytes-16          752743              1594 ns/op               0 B/op          0 allocs/op
        BenchmarkKvClaimBytes-16          763456              1581 ns/op               0 B/op          0 allocs/op
        BenchmarkKvClaimBytes-16          769056              1603 ns/op               0 B/op          0 allocs/op
        BenchmarkKvClaimBytes-16          762469              1613 ns/op               0 B/op          0 allocs/op
        BenchmarkKvClaimBytes-16          752967              1613 ns/op               0 B/op          0 allocs/op
        PASS
        ok      perkeep.org/pkg/index   12.208s
    
    Signed-off-by: Brad Fitzpatrick <brad@danga.com>

---
+++ pkg/escape/escape.go
@@ -0,0 +1,224 @@
+// Package escape contains functions for escaping and unescaping.
+//
+// It's largely copied from the Go standard library's net/url package,
+// with modifications to make it operate on byte slices.
+package escape
+
+import (
+	"strconv"
+)
+
+// QueryUnescapeAppend is like net/url.QueryUnescape, but appends the
+// unescaped result to the provided base slice, and returns the
+// resulting slice.
+//
+// It returns an error if any % is not followed by two hexadecimal
+// digits.
+func QueryUnescapeAppend(base, s []byte) ([]byte, error) {
+	return unescapeAppend(base, s, encodeQueryComponent)
+}
+
+type encoding int
+
+const (
+	encodePath encoding = 1 + iota
+	encodePathSegment
+	encodeHost
+	encodeZone
+	encodeUserPassword
+	encodeQueryComponent
+	encodeFragment
+)
+
+// unescape unescapes a string; the mode specifies
+// which section of the URL string is being unescaped.
+func unescapeAppend(base, s []byte, mode encoding) ([]byte, error) {
+	ret := base
+	// Count %, check that they're well-formed.
+	n := 0
+	hasPlus := false
+	for i := 0; i < len(s); {
+		switch s[i] {
+		case '%':
+			n++
+			if i+2 >= len(s) || !ishex(s[i+1]) || !ishex(s[i+2]) {
+				s = s[i:]
+				if len(s) > 3 {
+					s = s[:3]
+				}
+				return nil, EscapeError(s)
+			}
+			// Per https://tools.ietf.org/html/rfc3986#page-21
+			// in the host component %-encoding can only be used
+			// for non-ASCII bytes.
+			// But https://tools.ietf.org/html/rfc6874#section-2
+			// introduces %25 being allowed to escape a percent sign
+			// in IPv6 scoped-address literals. Yay.
+			if mode == encodeHost && unhex(s[i+1]) < 8 && string(s[i:i+3]) != "%25" {
+				return nil, EscapeError(s[i : i+3])
+			}
+			if mode == encodeZone {
+				// RFC 6874 says basically "anything goes" for zone identifiers
+				// and that even non-ASCII can be redundantly escaped,
+				// but it seems prudent to restrict %-escaped bytes here to those
+				// that are valid host name bytes in their unescaped form.
+				// That is, you can use escaping in the zone identifier but not
+				// to introduce bytes you couldn't just write directly.
+				// But Windows puts spaces here! Yay.
+				v := unhex(s[i+1])<<4 | unhex(s[i+2])
+				if string(s[i:i+3]) != "%25" && v != ' ' && shouldEscape(v, encodeHost) {
+					return nil, EscapeError(s[i : i+3])
+				}
+			}
+			i += 3
+		case '+':
+			hasPlus = mode == encodeQueryComponent
+			i++
+		default:
+			if (mode == encodeHost || mode == encodeZone) && s[i] < 0x80 && shouldEscape(s[i], mode) {
+				return nil, InvalidHostError(s[i : i+1])
+			}
+			i++
+		}
+	}
+
+	if n == 0 && !hasPlus {
+		return append(base, s...), nil
+	}
+
+	for i := 0; i < len(s); i++ {
+		switch s[i] {
+		case '%':
+			ret = append(ret, unhex(s[i+1])<<4|unhex(s[i+2]))
+			i += 2
+		case '+':
+			if mode == encodeQueryComponent {
+				ret = append(ret, ' ')
+			} else {
+				ret = append(ret, '+')
+			}
+		default:
+			ret = append(ret, s[i])
+		}
+	}
+	return ret, nil
+}
+
+func ishex(c byte) bool {
+	switch {
+	case '0' <= c && c <= '9':
+		return true
+	case 'a' <= c && c <= 'f':
+		return true
+	case 'A' <= c && c <= 'F':
+		return true
+	}
+	return false
+}
+
+func unhex(c byte) byte {
+	switch {
+	case '0' <= c && c <= '9':
+		return c - '0'
+	case 'a' <= c && c <= 'f':
+		return c - 'a' + 10
+	case 'A' <= c && c <= 'F':
+		return c - 'A' + 10
+	default:
+		panic("invalid hex character")
+	}
+}
+
+type EscapeError string
+
+func (e EscapeError) Error() string {
+	return "invalid URL escape " + strconv.Quote(string(e))
+}
+
+type InvalidHostError string
+
+func (e InvalidHostError) Error() string {
+	return "invalid character " + strconv.Quote(string(e)) + " in host name"
+}
+
+// Return true if the specified character should be escaped when
+// appearing in a URL string, according to RFC 3986.
+//
+// Please be informed that for now shouldEscape does not check all
+// reserved characters correctly. See golang.org/issue/5684.
+func shouldEscape(c byte, mode encoding) bool {
+	// §2.3 Unreserved characters (alphanum)
+	if 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z' || '0' <= c && c <= '9' {
+		return false
+	}
+
+	if mode == encodeHost || mode == encodeZone {
+		// §3.2.2 Host allows
+		//	sub-delims = "!" / "$" / "&" / "'" / "(" / ")" / "*" / "+" / "," / ";" / "="
+		// as part of reg-name.
+		// We add : because we include :port as part of host.
+		// We add [ ] because we include [ipv6]:port as part of host.
+		// We add < > because they're the only characters left that
+		// we could possibly allow, and Parse will reject them if we
+		// escape them (because hosts can't use %-encoding for
+		// ASCII bytes).
+		switch c {
+		case '!', '$', '&', '\'', '(', ')', '*', '+', ',', ';', '=', ':', '[', ']', '<', '>', '"':
+			return false
+		}
+	}
+
+	switch c {
+	case '-', '_', '.', '~': // §2.3 Unreserved characters (mark)
+		return false
+
+	case '$', '&', '+', ',', '/', ':', ';', '=', '?', '@': // §2.2 Reserved characters (reserved)
+		// Different sections of the URL allow a few of
+		// the reserved characters to appear unescaped.
+		switch mode {
+		case encodePath: // §3.3
+			// The RFC allows : @ & = + $ but saves / ; , for assigning
+			// meaning to individual path segments. This package
+			// only manipulates the path as a whole, so we allow those
+			// last three as well. That leaves only ? to escape.
+			return c == '?'
+
+		case encodePathSegment: // §3.3
+			// The RFC allows : @ & = + $ but saves / ; , for assigning
+			// meaning to individual path segments.
+			return c == '/' || c == ';' || c == ',' || c == '?'
+
+		case encodeUserPassword: // §3.2.1
+			// The RFC allows ';', ':', '&', '=', '+', '$', and ',' in
+			// userinfo, so we must escape only '@', '/', and '?'.
+			// The parsing of userinfo treats ':' as special so we must escape
+			// that too.
+			return c == '@' || c == '/' || c == '?' || c == ':'
+
+		case encodeQueryComponent: // §3.4
+			// The RFC reserves (so we must escape) everything.
+			return true
+
+		case encodeFragment: // §4.1
+			// The RFC text is silent but the grammar allows
+			// everything, so escape nothing.
+			return false
+		}
+	}
+
+	if mode == encodeFragment {
+		// RFC 3986 §2.2 allows not escaping sub-delims. A subset of sub-delims are
+		// included in reserved from RFC 2396 §2.2. The remaining sub-delims do not
+		// need to be escaped. To minimize potential breakage, we apply two restrictions:
+		// (1) we always escape sub-delims outside of the fragment, and (2) we always
+		// escape single quote to avoid breaking callers that had previously assumed that
+		// single quotes would be escaped. See issue #19917.
+		switch c {
+		case '!', '(', ')', '*':
+			return false
+		}
+	}
+
+	// Everything else must be escaped.
+	return true
+}
